58c1bc7c377ce705acabd052a35a1f43a12042a7
lixy <826446332@qq.com>
2021-05-26 13:06:42 +0800

    【修改】dict-mock

--- mock/api/system/dict.js
+++ mock/api/system/dict.js
@@ -6,10 +6,10 @@ const records = [];
 for (let i = 1; i < 5; ++i) {
     records.push({
         "id": Random.natural(1),
-        "type": `${ Random.ctitle(1, 3) }`,
-        "name": `${ Random.ctitle(1, 3) }`,
+        "type":  Random.ctitle() ,
+        "name":  Random.ctitle(),
         "disable": Random.boolean(),
-        "sort": Random.natural(5),
+        "sort": Random.natural(1, 3),
         "amount": Random.natural(5),
     });
 }
@@ -19,12 +19,12 @@ module.exports = {
     code: 200,
     data: {
         id:Random.natural(1),
-        name:`${ Random.ctitle(1,3) }`,
-        sort: Random.natural(5),
+        name:Random.ctitle(),
+        sort: Random.natural(1, 3),
         records: [{
             "id": Random.natural(1),
-            "type": `${ Random.ctitle(1, 3) }`,
-            "name": `${ Random.ctitle(1, 3) }`,
+            "type":  Random.ctitle() ,
+            "name":  Random.ctitle(),
             "disable": Random.boolean(),
             "sort": Random.natural(5),
             "amount": Random.natural(5),
