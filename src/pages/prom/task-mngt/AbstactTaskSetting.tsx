6655491400c6bd7667213b1b42ecaaa8112091ea
lixy <826446332@qq.com>
2021-06-01 10:51:40 +0800

    【修改】细节调整
    (cherry picked from commit 34b485a6c213d7800a01c8a73f51d1117f5df316)

--- src/pages/prom/task-mngt/AbstactTaskSetting.tsx
+++ src/pages/prom/task-mngt/AbstactTaskSetting.tsx
@@ -721,8 +721,7 @@
                             type="link" 
                             placement="topRight"
                             onConfirm={ () => {
-                                window.open('https://gimg2.baidu.com/image_search/src=http%3A%2F%2F1812.img.pp.sohu.com.cn%2Fimages%2Fblog%2F2009%2F11%2F18%2F18%2F8%2F125b6560a6ag214.jpg&refer=http%3A%2F%2F1812.img.pp.sohu.com.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1625048322&t=3b6da010e79ec1fcb5855fe9a69c3ceb')
-                            // <iframe src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2F1812.img.pp.sohu.com.cn%2Fimages%2Fblog%2F2009%2F11%2F18%2F18%2F8%2F125b6560a6ag214.jpg&refer=http%3A%2F%2F1812.img.pp.sohu.com.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1625048322&t=3b6da010e79ec1fcb5855fe9a69c3ceb" frameBorder="0"></iframe>
+                                console.log(record)
                             }}
                         >
                             <DeleteOutlined />
