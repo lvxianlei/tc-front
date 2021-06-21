71a7737118db84c68ae2975e398ea60526ea9022
lixy <826446332@qq.com>
2021-06-21 18:02:02 +0800

    【修改】任务单问题修复

--- src/pages/prom/task-mngt/TaskNew.tsx
+++ src/pages/prom/task-mngt/TaskNew.tsx
@@ -8,6 +8,7 @@
  import RequestUtil from '../../../utils/RequestUtil';
  import AbstractTaskSetting, { IAbstractTaskSettingState, StepItem } from './AbstactTaskSetting';
  import moment from 'moment'
+import { message } from 'antd';
  
  export interface ITaskNewProps {}
  export interface ITaskNewRouteProps extends RouteComponentProps<ITaskNewProps>, WithTranslation {}
@@ -37,13 +38,23 @@
       */
      public async onSubmit(values: Record<string, any>): Promise<void> {
         values.planDeliveryTime = moment(values.planDeliveryTime).format('YYYY-MM-DD');
-        values.deliveryTime = moment(values.deliveryTime).format('YYYY-MM-DD');
+        values.delproductIdsiveryTime = moment(values.deliveryTime).format('YYYY-MM-DD');
         values.signContractTime = moment(values.signContractTime).format('YYYY-MM-DD');
         values.productIds = this.state.selectedKeys.length > 0 ? this.state.selectedKeys : [];
         values.contractInfoDTO = this.state.contractInfoDTO;
         values.saleOrderId = this.state?.task?.saleOrderId;
         values.id = this.state?.task?.id;
-        return this.state.checkStep === StepItem.COMPLETE_PRODUCT_INFO ? await RequestUtil.post('/tower-market/taskNotice/saveAndSubApprove', values):await RequestUtil.post('/tower-market/taskNotice', values);
+        if(values.deliveryTime > values.signContractTime) {
+            message.error("客户交货日期应小于订单交货日期！")
+            return Promise.reject(false)
+        } 
+        else if(this.state.checkStep === StepItem.COMPLETE_PRODUCT_INFO && values.productIds == 0){
+            message.error("未选择产品信息，不可提交审批！")
+            return Promise.reject(false)
+        }
+        else {
+            return this.state.checkStep === StepItem.COMPLETE_PRODUCT_INFO ? await RequestUtil.post('/tower-market/taskNotice/saveAndSubApprove', values):await RequestUtil.post('/tower-market/taskNotice', values)
+        }
      }
  }
  
