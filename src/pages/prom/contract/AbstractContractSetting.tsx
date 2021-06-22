50f7928d46ee92d77a00020926d7afb415216b4a
zyc <757715809@qq.com>
2021-06-22 10:42:32 +0800

    新增合同业主联系电话去掉验证方法

--- src/pages/prom/contract/AbstractContractSetting.tsx
+++ src/pages/prom/contract/AbstractContractSetting.tsx
@@ -242,22 +242,6 @@ export default abstract class AbstractContractSetting<P extends RouteComponentPr
     }
 
     /**
-     * @description 验证业主联系电话格式
-     */
-    public checkcustomerPhone = (value: StoreValue): Promise<void | any> =>{
-        return new Promise(async (resolve, reject) => {  // 返回一个promise
-            const regPhone: RegExp = /^1[3|4|5|8][0-9]\d{8}$/;
-            const regTel: RegExp = /^\d{3}-\d{8}|\d{4}-\d{7}$/;
-            if(regPhone.test(value) || regTel.test(value) ) {
-                resolve(true)
-            } else 
-                resolve(false)
-        }).catch(error => {
-            Promise.reject(error)
-        })
-    }
-
-    /**
      * @description 验证合同编号是否重复
      */
     public checkContractNumber = (value: StoreValue): Promise<void | any> =>{
