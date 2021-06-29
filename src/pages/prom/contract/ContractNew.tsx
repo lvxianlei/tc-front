a57a7581ecf17271398900243dce28f4229ec8b0
zyc <757715809@qq.com>
2021-06-29 19:34:43 +0800

    【修改】回款计划占比，金额>0

--- src/pages/prom/contract/ContractNew.tsx
+++ src/pages/prom/contract/ContractNew.tsx
@@ -51,10 +51,16 @@ class ContractNew extends AbstractContractSetting<IContractNewRouteProps, IContr
             customerPhone: values.customerPhone
         };
         values.signCustomerId = this.state.contract?.signCustomerId;
-        if( values.planType === planType.PROPORTION && totalRate < 100 || totalRate > 100) {
+        if( totalRate < 100) {
             message.error('计划回款总占比必须等于100');
             return Promise.reject(false);
-        } else if( values.planType === planType.AMOUNT && totalAmount < values.contractAmount || totalAmount > values.contractAmount ) {
+        } else if( totalRate > 100 ) {
+            message.error('计划回款总占比必须等于100');
+            return Promise.reject(false);
+        } else if( totalAmount < values.contractAmount ) {
+            message.error('计划回款总金额必须等于合同总价');
+            return Promise.reject(false);
+        } else if( totalAmount > values.contractAmount ) {
             message.error('计划回款总金额必须等于合同总价');
             return Promise.reject(false);
         } else {
