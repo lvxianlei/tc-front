52459374d461611c2ebdcfe4be757693d06b9b21
zyc <757715809@qq.com>
2021-07-15 19:19:09 +0800

    【修改】审批mock修改

--- mock/api/tower-market/audit.js
+++ mock/api/tower-market/audit.js
@@ -1,6 +1,25 @@
 const Mock = require('mockjs');
 const Random = Mock.Random;
 
+const records = [];
+for (let i = 1; i < 10; ++i) {
+    records.push({
+        "auditStatus|0-2": 0,
+        "businessId": Random.natural(15),
+        "businessNumber": Random.natural(11),
+        "createTime": Random.datetime('yyyy年MM月dd日  HH:mm:ss'),
+        "createUser": Mock.mock('@cname()'),
+        "id": Mock.mock('@id()'),
+        "isDeleted": Random.boolean(1, 1, true),
+        "startTime": Random.datetime('yyyy年MM月dd日  HH:mm:ss'),
+        "tenantId": Random.integer(100),
+        "type|0-2": 0,
+        "typeName": Random.csentence(3, 5),
+        "updateTime": Random.datetime('yyyy年MM月dd日  HH:mm:ss'),
+        "updateUser": Mock.mock('@cname()')
+    });
+}
+
 module.exports = {
     "code": 200,
     "msg": "ea",
@@ -9,7 +28,7 @@ module.exports = {
         current: Random.natural(1, 20),
         records: [
             {
-                "auditStatus|0-2": 0,
+                "auditStatus": 0,
                 "businessId": Random.natural(15),
                 "businessNumber": Random.natural(11),
                 "createTime": Random.datetime('yyyy年MM月dd日  HH:mm:ss'),
@@ -18,12 +37,12 @@ module.exports = {
                 "isDeleted": Random.boolean(1, 1, true),
                 "startTime": Random.datetime('yyyy年MM月dd日  HH:mm:ss'),
                 "tenantId": Random.integer(100),
-                "type|0-2": 0,
+                "type": "SALE_ORDER_AUDIT",
                 "typeName": Random.csentence(3, 5),
                 "updateTime": Random.datetime('yyyy年MM月dd日  HH:mm:ss'),
                 "updateUser": Mock.mock('@cname()')
             }
-        ],
+        ].concat(records),
         current: Random.natural(1, 20),
         size: 10,
         total: Random.natural(1, 589),
