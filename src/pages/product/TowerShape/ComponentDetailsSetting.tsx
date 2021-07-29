b2ef802ee4bf1fa50615215388a7bcf40ab4e8cd
zyc <757715809@qq.com>
2021-07-29 16:19:15 +0800

    【修改】编辑构建明细长宽厚null传-1

--- src/pages/product/TowerShape/ComponentDetailsSetting.tsx
+++ src/pages/product/TowerShape/ComponentDetailsSetting.tsx
@@ -115,6 +115,12 @@ class ComponentDetailsSetting<P extends IComponentDetailsSettingRouteProps, S ex
         this.setState ({
             editingKey: undefined
         })
+        values = {
+            ...values,
+            width: values.width == null ? -1 : values.width,
+            thickness: values.thickness == null ? -1 : values.thickness,
+            length: values.length == null ? -1 : values.length,
+        }
         await RequestUtil.put('/tower-data-archive/drawComponent', values).then(res => {
             if(res) {
                 this.getData();
