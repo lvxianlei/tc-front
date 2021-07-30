f701bd2852cbf89864393b7cbcadf8f6e0f5a7a1
zyc <757715809@qq.com>
2021-07-30 11:24:43 +0800

    【修改】塔型编辑-杆塔传null问题

--- src/pages/product/TowerShape/TowerShapeSetting.tsx
+++ src/pages/product/TowerShape/TowerShapeSetting.tsx
@@ -37,15 +37,15 @@ class TowerShapeSetting extends AbstractTowerShapeSetting<ITowerShapeSettingRout
             return {
                 ...items,
                 productAdditionalDTOList: items.productAdditionalVOList,
-                towerLeg1Length: items.towerLeg1Length === -1 ? undefined : items.towerLeg1Length,
-                towerLeg1Weight: items.towerLeg1Weight === -1 ? undefined : items.towerLeg1Weight,
-                towerLeg2Length: items.towerLeg2Length === -1 ? undefined : items.towerLeg2Length,
-                towerLeg2Weight: items.towerLeg2Weight === -1 ? undefined : items.towerLeg2Weight,
-                towerLeg3Length: items.towerLeg3Length === -1 ? undefined : items.towerLeg3Length,
-                towerLeg3Weight: items.towerLeg3Weight === -1 ? undefined : items.towerLeg3Weight,
-                towerLeg4Length: items.towerLeg4Length === -1 ? undefined : items.towerLeg4Length,
-                towerLeg4Weight: items.towerLeg4Weight === -1 ? undefined : items.towerLeg4Weight,
-                towerFootWeight: items.towerFootWeight === -1 ? undefined : items.towerFootWeight,
+                towerLeg1Length: items.towerLeg1Length == -1 ? undefined : items.towerLeg1Length,
+                towerLeg1Weight: items.towerLeg1Weight == -1 ? undefined : items.towerLeg1Weight,
+                towerLeg2Length: items.towerLeg2Length == -1 ? undefined : items.towerLeg2Length,
+                towerLeg2Weight: items.towerLeg2Weight == -1 ? undefined : items.towerLeg2Weight,
+                towerLeg3Length: items.towerLeg3Length == -1 ? undefined : items.towerLeg3Length,
+                towerLeg3Weight: items.towerLeg3Weight == -1 ? undefined : items.towerLeg3Weight,
+                towerLeg4Length: items.towerLeg4Length == -1 ? undefined : items.towerLeg4Length,
+                towerLeg4Weight: items.towerLeg4Weight == -1 ? undefined : items.towerLeg4Weight,
+                towerFootWeight: items.towerFootWeight == -1 ? undefined : items.towerFootWeight,
             }
         })
         this.setState({
@@ -85,6 +85,15 @@ class TowerShapeSetting extends AbstractTowerShapeSetting<ITowerShapeSettingRout
             })  
             return {
                 ...items,
+                towerLeg1Length: items.towerLeg1Length === null ? -1 : items.towerLeg1Length,
+                towerLeg1Weight: items.towerLeg1Weight === null ? -1 : items.towerLeg1Weight,
+                towerLeg2Length: items.towerLeg2Length === null ? -1 : items.towerLeg2Length,
+                towerLeg2Weight: items.towerLeg2Weight === null ? -1 : items.towerLeg2Weight,
+                towerLeg3Length: items.towerLeg3Length === null ? -1 : items.towerLeg3Length,
+                towerLeg3Weight: items.towerLeg3Weight === null ? -1 : items.towerLeg3Weight,
+                towerLeg4Length: items.towerLeg4Length === null ? -1 : items.towerLeg4Length,
+                towerLeg4Weight: items.towerLeg4Weight === null ? -1 : items.towerLeg4Weight,
+                towerFootWeight: items.towerFootWeight === null ? -1 : items.towerFootWeight,
                 productAdditionalDTOList: productAdditionalDTOList,
                 productCategoryId: towerShape.id
             }
