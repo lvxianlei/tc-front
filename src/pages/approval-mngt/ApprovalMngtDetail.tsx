fbdc755f4bd4d1ee8b34ecf7a42f8a89d9297832
lvxianlei <lvxianlei@hotmail.com>
2021-09-14 11:13:10 +0800

    审批管理查询页完善

--- src/pages/approval-mngt/ApprovalMngtDetail.tsx
+++ src/pages/approval-mngt/ApprovalMngtDetail.tsx
@@ -1,115 +1,19 @@
 import React from 'react'
-import { Button, Popconfirm, Table, TableColumnProps } from 'antd'
+import { Row, Button, Table } from 'antd'
 import { useHistory } from 'react-router-dom'
-import { Detail } from '../common'
-import SummaryRenderUtil from '../../utils/SummaryRenderUtil'
-
-const tableColumns: TableColumnProps<Object>[] = [
-    { title: '序号', dataIndex: 'index', render: (_a: any, _b: any, index: number): React.ReactNode => (<span>{index + 1}</span>) },
-    {
-        title: '状态',
-        dataIndex: 'productStatus',
-        render: (productStatus: number): React.ReactNode => {
-            return productStatus === 1 ? '待下发' : productStatus === 2 ? '审批中' : '已下发'
-        }
-    },
-    { title: '线路名称', dataIndex: 'lineName' },
-    { title: '产品类型', dataIndex: 'productTypeName' },
-    { title: '塔型', dataIndex: 'productShape' },
-    { title: '杆塔号', dataIndex: 'productNumber' },
-    { title: '电压等级（KV）', dataIndex: 'voltageGradeName' },
-    { title: '呼高（米）', dataIndex: 'productHeight' },
-    { title: '单位', dataIndex: 'unit' },
-    {
-        title: '数量',
-        dataIndex: 'num',
-        render: (num: number | string): React.ReactNode => {
-            return num == -1 ? '' : num;
-        }
-    },
-    { title: '单价', dataIndex: 'price' },
-    { title: '金额', dataIndex: 'totalAmount' },
-    { title: '标段', dataIndex: 'tender' },
-    { title: '备注', dataIndex: 'description' }
-]
-
+import { Detail, DetailContent, BaseInfo, CommonTable } from '../common'
+import { enclosure, baseInfo } from './approvalHeadData.json'
 export default function ApprovalMngtDetail(): React.ReactNode {
     const history = useHistory()
-    const tabItems = [
-        {
-            label: '概况信息',
-            key: 1,
-            content: SummaryRenderUtil.renderSections([
-                {
-                    title: '基本信息',
-                    render: () => SummaryRenderUtil.renderGrid({
-                        labelCol: { span: 4 },
-                        valueCol: { span: 8 },
-                        rows: [
-                            [{
-                                label: '项目名称',
-                                value: 'baseInfo?.contractNumber'
-                            }, {
-                                label: '项目单位',
-                                value: 'baseInfo?.internalNumber'
-                            }], [{
-                                label: '项目编号',
-                                value: 'baseInfo?.projectName'
-                            }, {
-                                label: '数量',
-                                value: 'baseInfo?.simpleProjectName'
-                            }], [{
-                                label: '单位',
-                                value: 'baseInfo?.winBidTypeName'
-                            }, {
-                                label: '货物类别',
-                                value: 'baseInfo?.saleTypeName'
-                            }], [{
-                                label: '招标文件传递方式',
-                                value: 'baseInfo?.customerInfoVo?.customerCompany'
-                            }, {
-                                label: '价格范围（元/吨）',
-                                value: 'baseInfo?.customerInfoVo?.customerLinkman'
-                            }], [{
-                                label: '验收执行标准及验收方法',
-                                value: 'baseInfo?.customerInfoVo?.customerPhone'
-                            }, {
-                                label: '原材料执行标准',
-                                value: 'baseInfo?.signCustomerName'
-                            }], [{
-                                label: '包装要求',
-                                value: 'baseInfo?.signContractTime'
-                            }, {
-                                label: '是否有合同版本',
-                                value: 'baseInfo?.signUserName'
-                            }], [{
-                                label: '货款结算条件及方式',
-                                value: 'baseInfo?.deliveryTime'
-                            }, {
-                                label: '特殊材质',
-                                value: 'baseInfo?.reviewTime'
-                            }], [{
-                                label: '其他',
-                                value: 'baseInfo?.countryCode'
-                            }, {
-                                label: '审批状态',
-                                value: 'baseInfo?.regionName'
-                            }], [{
-                                label: '备注',
-                                value: 'baseInfo?.chargeType === ChargeType.ORDER_TOTAL_WEIGHT'
-                            }]
-                        ]
-                    })
-                },
-                {
-                    title: '附件信息',
-                    render: () => <Table columns={tableColumns} />
-                },
-                { title: '审批记录', render: () => <Table columns={tableColumns} /> }
-            ])
-        }]
-
-    return <Detail
+    console.log(enclosure)
+    return <DetailContent
         operation={[<Button key="new" onClick={() => history.goBack()}>返回</Button>]}
-        tabItems={tabItems} />
+    >
+        <Row>基本信息</Row>
+        <BaseInfo columns={baseInfo} dataSource={[]} />
+        <Row>附件信息</Row>
+        <CommonTable columns={enclosure} />
+        <Row>审批记录</Row>
+        <CommonTable columns={enclosure} />
+    </DetailContent>
 }
--- src/pages/common/CommonTable.tsx
+++ src/pages/common/CommonTable.tsx
@@ -2,37 +2,51 @@ import React from "react"
 import { Table, TableColumnProps } from "antd"
 import styles from "./CommonTable.module.less"
 import moment from "moment"
-type FormItemTypesType = "text" | "number" | "select" | "date" | undefined
+type ColumnsItemsType = "text" | "string" | "number" | "select" | "date" | undefined
 
-function generateRender(type: FormItemTypesType, data: ColumnsItem) {
+function generateRender(type: ColumnsItemsType, data: (SelectData | TextData)) {
     switch (type) {
         case "date":
-            return ({ ...data, expect: true, onCell: () => ({ className: styles.tableCell }), render: (text: string, record: any) => <>{moment(text, record.format || "YYYY-MM-DD HH:mm:ss")}</> })
+            return ({ ...data, ellipsis: true, onCell: () => ({ className: styles.tableCell }), render: (text: string, record: any) => <>{moment(text, record.format || "YYYY-MM-DD HH:mm:ss")}</> })
         case "select":
-            return ({ ...data, expect: true, onCell: () => ({ className: styles.tableCell }), render: (text: string, record: any) => <>{record.enum ? record.enum.find((item: { value: string, label: string }) => item.value === text).label : text}</> })
+            return ({ ...data, ellipsis: true, onCell: () => ({ className: styles.tableCell }), render: (text: string, record: any) => <>{record.enum ? record.enum.find((item: { value: string, label: string }) => item.value === text).label : text}</> })
         default:
-            return ({ ...data, expect: true, onCell: () => ({ className: styles.tableCell }) })
+            return ({ ...data, ellipsis: true, onCell: () => ({ className: styles.tableCell }) })
     }
 }
-
 
 interface ColumnsItem {
     title: string
     dataIndex: string
-    type?: FormItemTypesType
-    key?: string
-    rowKey?: string
-    width?: string | number
-    render?: (text: string, record: any, index: number) => JSX.Element | React.ReactNode
+    type?: ColumnsItemsType
+    [key: string]: any
+}
+
+interface SelectOption {
+    value: string | number
+    label: string
+    [key: string]: any
+}
+
+interface SelectData extends ColumnsItem {
+    type: "select"
+    enum?: SelectOption[]
+    path?: string
+    [key: string]: any
+}
+
+interface TextData extends ColumnsItem {
+    type?: "text" | undefined
+    [key: string]: any
 }
 
 interface CommonTableProps {
-    columns: ColumnsItem[]
+    columns: (SelectData | TextData)[]
     dataSource?: object[]
 }
 
 export default function CommonTable({ columns, dataSource = [] }: CommonTableProps): JSX.Element {
-    columns = columns.map((item: ColumnsItem, index: number) => generateRender(item.type || "text", item))
+    columns = columns.map((item: (SelectData | TextData), index: number) => generateRender(item.type || "text", item))
     return <Table
         size="small"
         rowKey={(record: any) => `common_table_${record.id || record.title || record.dataIndex}`}
