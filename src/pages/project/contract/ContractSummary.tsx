afdec6f8ab8602d33aab572f654e6f32e0775129
wangtiechuan <wangtiechuanmail@163.com>
2021-09-16 15:16:58 +0800

    合同详情内的订单列表接入接口

--- src/pages/project/contract/ContractSummary.tsx
+++ src/pages/project/contract/ContractSummary.tsx
@@ -10,6 +10,7 @@ import { IProduct } from "../../IProduct";
 import RequestUtil from "../../../utils/RequestUtil";
 
 class ManagementContractSummary extends ContractSummary {
+  requestPath = "/tower-market/saleOrder";
   getBaseInfoGrid() {
     const baseInfo: any | undefined = this.state.baseInfo;
     return {
