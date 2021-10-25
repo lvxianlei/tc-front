c2937938f802d97d8a05dec9d73a352f6ef0394b
zyc <757715809@qq.com>
2021-10-25 19:33:05 +0800

    【开发】工作台

---
+++ mock/api/tower-science/workbench.js
@@ -0,0 +1,47 @@
+
+module.exports = {
+    "code": 200,
+    "msg":"dolor occaeca",
+    "success":false,
+    "data":{
+        "director":{
+            // "assessToBeAssigned": 1,
+            "assessToBeConfirmed": 5,
+            "assessToBeSubmitted": 5,
+            "confirmToBeAssigned": 46,
+            "confirmToBeConfirmed":8,
+            "confirmToBeSubmitted":23,
+            "examineAndApprove":7,
+            "loftingToBeAssigned":9,
+            "loftingToBeConfirmed":2,
+            "loftingToBeSubmitted":0,
+            "questionnaire":3
+        },
+        "leaderToDoVO":{
+            "loftingToBeAssigned":7,
+            "loftingToBeSubmittedLofting":7,
+            "loftingToBeSubmittedWelding":5,
+            "loftingToBeSummarized":4,
+            "materialToBeAssigned":1,
+            "materialToBeSubmittedMaterial":51,
+            "materialToBeSummarized":5
+        },
+        "staffToDoVO":{
+            // "assessToBeComplete":7,
+            // "boltToBeCheck":10,
+            // "boltToBeMade":5,
+            // "confirmToBeComplete":1,
+            // "loftingToBeCheck":5,
+            // "loftingToBeIssue":9,
+            // "loftingToBeLofting":5,
+            // "loftingToBeMatch":4,
+            // "materialToBeCheck":2,
+            // "materialToBeMatch":4,
+            // "materialToBeMaterial":5,
+            // "sampleToBeCheck":1,
+            // "sampleToBeUploaded":2,
+            // "weldingToBeCheck":7,
+            // "weldingToBeWelding":5
+        }
+    }
+}
---
+++ src/pages/workBench/Line.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { WithTranslation, withTranslation } from 'react-i18next';
+import { RouteComponentProps, withRouter } from 'react-router';
+import * as echarts from 'echarts';
+import styles from './WorkBench.module.less';
+import { DetailTitle } from '../common';
+
+export interface LineProps {}
+export interface ILineRouteProps extends RouteComponentProps<LineProps>, WithTranslation {
+    readonly keyIndex: string;
+    readonly valueList: (string| number)[];
+}
+
+export interface LineState {}
+
+const dateList = [1, 2, 3, 4, 5, 6, 7 ];
+
+class Line extends React.Component<ILineRouteProps, LineState> {
+
+    componentDidMount() {
+        this.initCharts();
+    }
+
+	protected initCharts() {
+		const myChart = echarts.init((document as HTMLElement | any).getElementById(this.props.keyIndex || 'main'));
+        // 绘制图表
+        myChart.setOption({ 
+			color: '#FF8C00',
+            tooltip: { show: false },
+            xAxis: [{
+				data: dateList,
+				show: false,
+                boundaryGap: false
+            }],
+            yAxis: [{ show: false }],
+            grid: { x: 10, y: 10, y2: 10 },
+            series: [{
+				type: 'line',
+				showSymbol: false,
+				data: this.props.valueList
+            }]
+        });
+	}
+
+    /**
+     * @description Renders AbstractDetailComponent
+     * @returns render 
+     */
+    public render(): React.ReactNode {
+        return <div id={ this.props.keyIndex || 'main'} style={{ width:'100%', height:'60px', background:'#F8F8F8' }} key={ this.props.keyIndex }></div>
+    }
+}
+
+export default withRouter(withTranslation('translation')(Line))
