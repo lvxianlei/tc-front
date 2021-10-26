4c7cdecbd9a5f126e8e646657c468c5930659d90
“lujing” <lj_lujing@outlook.com>
2021-10-26 14:05:21 +0800

    添加回款付款。。。

---
+++ src/pages/Payment/index.tsx
@@ -0,0 +1,98 @@
+import React from "react"
+import { Button, Input, DatePicker, Select, Modal, message } from 'antd'
+import { Link, useHistory } from 'react-router-dom'
+import { Page } from '../common'
+import { PaymentListHead } from "./PaymentData.json"
+import useRequest from '@ahooksjs/use-request'
+import RequestUtil from '../../utils/RequestUtil'
+export default function Payment() {
+    const history = useHistory()
+
+    const { loading, run: deleteRun } = useRequest<{ [key: string]: any }>((id: string) => new Promise(async (resole, reject) => {
+        try {
+            const result: { [key: string]: any } = await RequestUtil.delete(`/tower-market/Payment?id=${id}`)
+            resole(result)
+        } catch (error) {
+            reject(error)
+        }
+    }), { manual: true })
+
+    const onFilterSubmit = (value: any) => {
+        if (value.startLaunchTime) {
+            const formatDate = value.startLaunchTime.map((item: any) => item.format("YYYY-MM-DD"))
+            value.startLaunchTime = formatDate[0]
+            value.endLaunchTime = formatDate[1]
+        }
+        return value
+    }
+
+    const handleDelete = (id: string) => {
+        Modal.confirm({
+            title: "删除",
+            content: "确定删除此开票申请吗？",
+            onOk: () => new Promise(async (resove, reject) => {
+                try {
+                    resove(await deleteRun(id))
+                    message.success("删除成功...")
+                    history.go(0)
+                } catch (error) {
+                    reject(error)
+                }
+            })
+        })
+    }
+
+    return <Page
+        path="/tower-market/Payment"
+        columns={[
+            ...PaymentListHead,
+            {
+                title: "操作",
+                dataIndex: "opration",
+                fixed: "right",
+                width: 100,
+                render: (_: any, record: any) => {
+                    return <>
+                        <Button type="link" onClick={() => history.push(`/project/Payment/detail/${record.id}`)}>查看</Button>
+                        <Button type="link" onClick={() => history.push(`/project/Payment/edit/${record.id}`)}>编辑</Button>
+                        <Button type="link" onClick={() => handleDelete(record.id)}>删除</Button>
+                    </>
+                }
+            }]}
+        extraOperation={<Link to="/project/Payment/edit/new"><Button type="primary">新增开票申请</Button></Link>}
+        onFilterSubmit={onFilterSubmit}
+        searchFormItems={[
+            {
+                name: 'fuzzyQuery',
+                children: <Input placeholder="编号/内部合同编号/工程名称/票面单位/业务经理" style={{ width: 300 }} />
+            },
+            {
+                name: 'isOpen',
+                label: '是否已全开',
+                children: <Select style={{ width: 200 }}>
+                    <Select.Option value="a">全部</Select.Option>
+                    <Select.Option value="b">预开</Select.Option>
+                    <Select.Option value="c">发票已开全</Select.Option>
+                    <Select.Option value="d">发票未开全</Select.Option>
+                </Select>
+            },
+            {
+                name: 'contractType',
+                label: '开票时合同状态',
+                children: <Select style={{ width: 200 }}>
+                    <Select.Option value="a">全部</Select.Option>
+                    <Select.Option value="b">不下计划</Select.Option>
+                    <Select.Option value="c">未下计划</Select.Option>
+                    <Select.Option value="d">未下完计划</Select.Option>
+                    <Select.Option value="e">未发完货</Select.Option>
+                    <Select.Option value="f">已发完货</Select.Option>
+                </Select>
+            },
+            {
+                name: 'startLaunchTime',
+                label: '申请日期',
+                children: <DatePicker.RangePicker format="YYYY-MM-DD" />
+            }
+        ]}
+    />
+}
