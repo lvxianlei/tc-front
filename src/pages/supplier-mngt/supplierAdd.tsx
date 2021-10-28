6bce4f8e2e817ad2ff53e010db0d9989ab18104d
zhanghua <18231209794@163.com>
2021-10-28 18:47:22 +0800

    冲突，部分接口调试

--- src/pages/supplier-mngt/supplierAdd.tsx
+++ src/pages/supplier-mngt/supplierAdd.tsx
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from 'react'
-import { Button, Spin, Space, Modal, Form, message, Image, Descriptions } from 'antd';
-import { AddEditDetail } from "./supplier-mngt.json"
-import { Page } from '../common';
-import useRequest from '@ahooksjs/use-request';
-import RequestUtil from '../../utils/RequestUtil';
-
-//post请求创建
-
-export default function SupplierAdd(): React.ReactNode {
-    return (
-        <div>
-            <Descriptions title="供应商基础信息" bordered column={2} labelStyle={{ textAlign: 'center' }}>
-                <Descriptions.Item label="供应商编号">Zhou Maomao</Descriptions.Item>
-                <Descriptions.Item label="供应商名称 *">Zhou Maomao</Descriptions.Item>
-                <Descriptions.Item label="供应商类型 *">Zhou Maomao</Descriptions.Item>
-                <Descriptions.Item label="质量保证体系 *">Zhou Maomao</Descriptions.Item>
-                <Descriptions.Item label="联系人 *">Zhou Maomao</Descriptions.Item>
-                <Descriptions.Item label="联系电话 *">Zhou Maomao</Descriptions.Item>
-                <Descriptions.Item label="主要供货产品 *">Zhou Maomao</Descriptions.Item>
-                <Descriptions.Item label="备注">Zhou Maomao</Descriptions.Item>
-            </Descriptions>
-            <Descriptions title="供应商账户信息" bordered column={2} labelStyle={{ textAlign: 'center' }}>
-                <Descriptions.Item label="开户银行 *">Zhou Maomao</Descriptions.Item>
-                <Descriptions.Item label="银行账号 *">Zhou Maomao</Descriptions.Item>
-            </Descriptions>
-            <Page
-                path="/tower-supply/supplier"
-                columns={
-                    [
-                        ...AddEditDetail
-                    ]
-                }
-                searchFormItems={[]}
-            />
-        </div>
-    )
-=======
 import React, { useState } from 'react'
 import { Button, Spin, Space, Modal, Form, message, Image } from 'antd';
 import { useHistory, useParams } from 'react-router-dom';
@@ -51,5 +12,4 @@ export default function SupplierAdd(): React.ReactNode {
             123
         </div>
     )
->>>>>>> 25ed075f42dd4f88cf9cc91386a97f3540db2079
 }
