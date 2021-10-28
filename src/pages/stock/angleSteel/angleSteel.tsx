95928ed6bb4cb90710e326cd04f9283b048a98ab
“lujing” <lj_lujing@outlook.com>
2021-10-28 18:12:27 +0800

    角钢配置策略修改

--- src/pages/stock/angleSteel/angleSteel.tsx
+++ src/pages/stock/angleSteel/angleSteel.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react'
-import { Button, Col, Pagination, Row, Select, TableColumnProps, Table, } from 'antd'
+import { Button, Col, Pagination, Row, Select, TableColumnProps, Table } from 'antd'
+import { CommonTable } from '../../common'
 import RequestUtil from "../../../utils/RequestUtil"
 import { RouteProps } from '../public'
 const { Option } = Select;
@@ -43,64 +44,6 @@ const AngleSteel = (props: RouteProps) => {
             dataIndex: 'operation',
             align: 'center',
         }]
-        // const column: TableColumnProps<object>[] = [
-        //     {
-        //         key: 'index',
-        //         title: '序号',
-        //         dataIndex: 'index',
-        //         width: 50,
-        //         render: (text, item, index) => {
-        //             return <span>{index + 1}</span>
-        //         }
-        //     },
-        //     {
-        //         key: 'materialTexture',
-        //         title: '材质',
-        //         dataIndex: 'materialTexture',
-        //     },
-        //     {
-        //         key: 'thickness',
-        //         title: "厚度mm",
-        //         dataIndex: "thickness",
-        //     },
-        //     {
-        //         key: 'width',
-        //         title: "宽度mm",
-        //         dataIndex: "width",
-        //     },
-        //     {
-        //         key: 'clampLoss',
-        //         title: "夹钳总耗损mm",
-        //         dataIndex: "clampLoss",
-        //     },
-        //     {
-        //         key: 'edgeLoss',
-        //         title: "每刀口耗损mm",
-        //         dataIndex: "edgeLoss",
-        //     },
-        //     {
-        //         key: 'description',
-        //         title: "备注",
-        //         dataIndex: "description",
-        //     },
-        //     {
-        //         key: 'operation',
-        //         title: '操作',
-        //         dataIndex: 'operation',
-        //         align: 'center',
-        //         render: (_text: any, item: any, index: number): React.ReactNode => {
-        //             return (
-        //                 <div className='operation'>
-        //                     <span
-        //                         onClick={() => {
-        //                             setIsModal(true)
-        //                             setId(item.id)
-        //                         }}
-        //                     >编辑</span>
-        //                 </div>
-        //             )
-        //         }
-        //     }]
     useEffect(() => {
         getColumnsData()
     }, [current, size]);
@@ -119,30 +62,9 @@ const AngleSteel = (props: RouteProps) => {
     return (
         <div className='public_page'>
             <div className='public_content'>
-                <div className='func_box'>
-                    <div className='func'>
-                    <span>配料基础配置</span>
-                    </div>
-                    <div className='func_right'>
-                        <Button
-                            className='func_right_item'
-                            onClick={() => {
-                                setIsModal(true)
-                                setId(null)
-                            }}
-                        >添加</Button>
-                        <Button
-                            className='func_right_item'
-                        >返回上一级</Button>
-                    </div>
-                </div>
-                <Table
-                    className='public_table'
-                    scroll={{ x: true }}
+                <CommonTable         
                     columns={columns}
                     dataSource={columnsData}
-                    pagination={false}
-                    size='small'
                 />
                 <div className='page_content'>
                     <Pagination
@@ -159,11 +81,11 @@ const AngleSteel = (props: RouteProps) => {
                     />
                 </div>
             </div>
-          
+
         </div>
     )
 
-    
+
 }
 
 export default AngleSteel;
