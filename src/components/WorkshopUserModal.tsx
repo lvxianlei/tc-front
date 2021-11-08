40b9e647cc131a087eca00a07047948eb4e8b02b
zyc <757715809@qq.com>
2021-11-08 18:21:24 +0800

    【联调】选择用户弹窗

--- src/components/WorkshopUserModal.tsx
+++ src/components/WorkshopUserModal.tsx
@@ -4,46 +4,51 @@
  */
 import React from 'react';
 import { Button, FormItemProps, Input, Modal, Space } from 'antd';
-import { ColumnType, TablePaginationConfig } from 'antd/lib/table';
+import { ColumnType, TablePaginationConfig, TableProps } from 'antd/lib/table';
 import { GetRowKey } from 'rc-table/lib/interface';
 import RequestUtil from '../utils/RequestUtil';
 import { IAbstractSelectableModalProps, IAbstractSelectableModalState, IResponseData } from './AbstractSelectableModal';
 import styles from './AbstractSelectableModal.module.less';
 import AbstractFilteredSelecableModal from './AbstractFilteredSelecableModal';
+import { ButtonType } from 'antd/lib/button';
+import { RowSelectionType } from 'antd/lib/table/interface';
 
 export interface IWorkshopUserSelectionComponentState extends IAbstractSelectableModalState {
-    readonly tableDataSource: IOrder[];
+    readonly tableDataSource: IUser[];
 }
 export interface IWorkshopUserSelectionComponentProps extends IAbstractSelectableModalProps {
     readonly saleOrderId?: string | number;
+    readonly buttonType?: ButtonType;
+    readonly buttonTitle?: string;
+    readonly rowSelectionType?: RowSelectionType | undefined;
 }
 
 export interface IResponseDataMore extends IResponseData {
-    readonly orderProductVos: [];
+    readonly IUser: [];
 }
-export interface IOrder {
-    readonly description: string;
-    readonly lineName: string;
-    readonly num: number;
-    readonly price: number;
-    readonly productHeight: number;
-    readonly productNumber: string;
-    readonly productCategoryName: string;
-    readonly productStatus: number;
-    readonly productType: number;
-    readonly productTypeName: string;
-    readonly saleOrderId: number;
-    readonly taskNoticeId: string;
-    readonly tender: string;
-    readonly totalAmount: number;
-    readonly unit: string;
-    readonly voltageGrade: number;
-    readonly status: number;
+
+export interface IUser {
     readonly id: string;
+    readonly account: string;
+    readonly clientId: string;
+    readonly createTime: string;
+    readonly createUser: string;
+    readonly departmentId: string;
+    readonly departmentName: string;
+    readonly email: string;
+    readonly name: string;
+    readonly openId: string;
+    readonly phone: string;
+    readonly tenantId: string;
+    readonly tenantName: string;
+    readonly userRoleNames: string;
+    readonly stationName: string;
+    readonly stationStatus: string;
+    readonly userRoleList: string;
 }
 
 /**
- * Order Selection Component
+ * Workshop User Selection Component
  */
 export default class WorkshopUserSelectionComponent extends AbstractFilteredSelecableModal<IWorkshopUserSelectionComponentProps, IWorkshopUserSelectionComponentState> {
 
@@ -65,6 +70,17 @@ export default class WorkshopUserSelectionComponent extends AbstractFilteredSele
         };
     }
 
+    public getTableProps() {
+        return {
+            ...super.getTableProps(),
+            rowSelection: {
+                type: this.props.rowSelectionType || 'radio',
+                selectedRowKeys: this.state.selectedRowKeys,
+                onChange: this.onSelectChange
+            }
+        }
+    }
+
     //componentDidMount
     public componentDidMount(): void {
         this.getTable({})
@@ -72,14 +88,15 @@ export default class WorkshopUserSelectionComponent extends AbstractFilteredSele
 
     //接口、获值
     public async getTable(filterValues: Record<string, any>, pagination: TablePaginationConfig = {}) {
-        let resData: IOrder[] = await RequestUtil.get<IOrder[]>(`/tower-market/saleOrder/orderProduct/${this.props.saleOrderId}`);
+        let resData: IResponseData = await RequestUtil.get<IResponseData>(`/sinzetech-user/user`);
         const selectKeys: [] = this.props.selectKey;
-        selectKeys.forEach((item: IOrder) => {
-            resData = resData.filter(res => res.id !== item.id);
+        let newData: IUser[] = resData.records;
+        selectKeys?.forEach((item: IUser) => {
+            newData = newData.filter(res => res.id !== item.id);
         })
         this.setState({
             ...filterValues,
-            tableDataSource: resData,
+            tableDataSource: newData,
         });
     }
 
@@ -113,20 +130,28 @@ export default class WorkshopUserSelectionComponent extends AbstractFilteredSele
             width: '15%',
             dataIndex: 'name'
         }, {
-            key: 'linkman',
+            key: 'departmentName',
             title: '部门',
             width: '15%',
-            dataIndex: 'linkman'
+            dataIndex: 'departmentName'
         }, {
-            key: 'phone',
+            key: 'stationName',
             title: '岗位',
             width: '15%',
-            dataIndex: 'phone'
+            dataIndex: 'stationName'
         }, {
-            key: 'phone',
+            key: 'stationStatus',
             title: '在职状态',
             width: '15%',
-            dataIndex: 'phone'
+            dataIndex: 'stationStatus',
+            render: (stationStatus: number): React.ReactNode => {
+                switch (stationStatus) {
+                    case 0:
+                        return '不在职';
+                    case 1:
+                        return '在职';
+                }
+            }  
         }, {
             key: 'phone',
             title: '联系电话',
@@ -140,7 +165,6 @@ export default class WorkshopUserSelectionComponent extends AbstractFilteredSele
         return 'id';
     }
 
-
     public showModal = (): void => {
         if (this.props.saleOrderId) {
             this.setState({
@@ -151,15 +175,14 @@ export default class WorkshopUserSelectionComponent extends AbstractFilteredSele
 
     }
 
-
     public render(): React.ReactNode {
         return (
             <>
-                <Button type="primary" onClick={()=>{
+                <Button type={this.props.buttonType || 'primary'} onClick={()=>{
                     this.setState({
                         isModalVisible: true
                     })
-                }}>添加人员</Button>
+                }}>{this.props.buttonTitle || '添加人员'}</Button>
                 <Modal
                     title={this.state.confirmTitle}
                     visible={this.state.isModalVisible}
