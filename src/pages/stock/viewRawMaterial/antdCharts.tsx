91c362aa0689b532e0fc1348dd847652d31b3215
张泽轩 <1844850686@qq.com>
2021-11-10 21:58:34 +0800

    bug修改

--- src/pages/stock/viewRawMaterial/antdCharts.tsx
+++ src/pages/stock/viewRawMaterial/antdCharts.tsx
@@ -1,51 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Line } from '@ant-design/charts';
 
 interface IProps {
-  result: any
+  results: any
+  i:any
 }
-
-// const Page: React.FC = () => {
-//   const data = [
-//     { year: '5号', value: 70 },
-//     { year: '10号', value: 140 },
-//     { year: '15号', value: 90 },
-//     { year: '20号', value: 180 },
-//     { year: '25号', value: 40 },
-//     { year: '30号', value: 210 },
-//   ];
-
-//   const config = {
-//     data,
-//     height: 400,
-//     xField: 'year',
-//     yField: 'value',
-//     point: {
-//       size: 5,
-//       shape: 'diamond',
-//     },
-//   };
-//   return <Line {...config} />;
-// };
-// export default Page;
-
+ 
 export default function Page(props: any) {
-  const result = props.result
-  console.log(result);
-  const data = [
-    { year: '5号', value: 70 },
-    { year: '10号', value: 140 },
-    { year: '15号', value: 90 },
-    { year: '20号', value: 180 },
-    { year: '25号', value: 40 },
-    { year: '30号', value: 210 },
-  ];
+  const [o,setO]=useState<any>({});
+  const results = props
+  console.log(results, 'a1');
 
+  var arr1 = []
+  for (let i in results) {
+    o[i] = results[i];
+    arr1.push(o)
+  }
+  console.log(arr1[0].arr,'a2');
+
+  const data=arr1[0].arr
+  console.log(data,12345);
+  
   const config = {
     data,
     height: 400,
-    xField: 'year',
-    yField: 'value',
+    xField: "updateTime",
+    yField: "price",
     point: {
       size: 5,
       shape: 'diamond',
