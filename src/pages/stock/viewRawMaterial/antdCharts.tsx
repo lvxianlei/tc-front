5e697a1c6b6bf18260267ac47471c2d580e2cd96
张泽轩 <1844850686@qq.com>
2021-11-12 09:45:00 +0800

    bug修改

--- src/pages/stock/viewRawMaterial/antdCharts.tsx
+++ src/pages/stock/viewRawMaterial/antdCharts.tsx
@@ -9,18 +9,13 @@ interface IProps {
 export default function Page(props: any) {
   const [o,setO]=useState<any>({});
   const results = props
-  console.log(results, 'a1');
 
   var arr1 = []
   for (let i in results) {
     o[i] = results[i];
     arr1.push(o)
   }
-  console.log(arr1[0].arr,'a2');
-
   const data=arr1[0].arr
-  console.log(data,12345);
-  
   const config = {
     data,
     height: 400,
