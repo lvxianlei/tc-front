51e9f9d3886f11608258687e1a346645c8cfecc8
lixy <826446332@qq.com>
2021-11-17 16:51:34 +0800

    【开发】包装清单

---
+++ src/pages/mngt-workshop/processingTask/ProcessDetail.tsx
@@ -0,0 +1,120 @@
+import React, { useState } from 'react'
+import { Button, Form, message, Modal, Popconfirm, Space, Spin, Table } from 'antd';
+import { useHistory, useParams } from 'react-router-dom';
+import { BaseInfo, DetailContent, CommonTable, DetailTitle } from '../../common';
+import { baseInfoData } from './detail.json';
+import useRequest from '@ahooksjs/use-request';
+import RequestUtil from '../../../utils/RequestUtil';
+import WorkshopUserSelectionComponent, { IUser } from '../../../components/WorkshopUserModal';
+
+const tableColumns = [
+    { title: '工序', dataIndex: 'index', key: 'index', render: (_a: any, _b: any, index: number): React.ReactNode => (<span>{index + 1}</span>) },
+    { title: '产线', dataIndex: 'createDeptName', key: 'createDeptName', },
+    { title: '派工设备', dataIndex: 'createUserName', key: 'createUserName' },
+    { title: '班组', dataIndex: 'createTime', key: 'createTime' },
+    // { title: '任务状态', dataIndex: 'status', key: 'status',  render: (value: number, record: object): React.ReactNode => {
+    //     const renderEnum: any = [
+    //         {
+    //             value: 0,
+    //             label: "已拒绝"
+    //         },
+    //         {
+    //             value: 1,
+    //             label: "待修改"
+    //         },
+    //         {
+    //             value: 2,
+    //             label: "已修改"
+    //         },
+    //         {
+    //             value: 3,
+    //             label: "已删除"
+    //         },
+    //     ]
+    //     return <>{renderEnum.find((item: any) => item.value === value).label}</>
+    // }},
+    { title: '完成时间', dataIndex: 'description', key: 'description' },
+    { title: '状态', dataIndex: 'description', key: 'description' }
+]
+
+export default function ProcessDetail(): React.ReactNode {
+    const history = useHistory();
+    const [form] = Form.useForm();
+    const params = useParams<{ id: string, status: string }>();
+    const [visible, setVisible] = useState<boolean>(false);
+    const [tableDataSource,setTableDataSource] = useState<any>([]);
+    const [userDataSource,setUserDataSource] = useState<any>([]);
+    const { loading, data } = useRequest(() => new Promise(async (resole, reject) => {
+        // let data = await RequestUtil.get(``,{})
+        resole(data)
+    }), {})
+    const detailData: any = data;
+    const delRow = (index: number) => {
+        userDataSource.splice(index, 1);
+        setUserDataSource([...userDataSource]);
+    }
+    return <>
+        <Spin spinning={loading}>
+            <DetailContent operation={params.status!=='3'?[
+                <Button key="edit" style={{ marginRight: '10px' }} type="primary" onClick={async () => {
+                    setVisible(true)
+                }}>数据采集</Button>,
+                <Button key="goback" onClick={() => history.goBack()}>返回</Button>
+            ]:[
+                <Button key="goback" onClick={() => history.goBack()}>返回</Button>
+            ]}>
+                <DetailTitle title="部件详情" />
+                <BaseInfo columns={baseInfoData} dataSource={detailData || {}}/>
+                <DetailTitle title="当前进度" />
+                <CommonTable 
+                    columns={tableColumns}
+                    dataSource={tableDataSource} 
+                    pagination={false}
+                />
+            </DetailContent>
+            <Modal
+                visible={ visible } 
+                width="40%" 
+                title="采集确认-选择员工"
+                footer={ <Space>
+                    <Button type="primary" ghost  onClick={() => setVisible(false) }>取消</Button>
+                    <Button type="primary" onClick={async () => {
+                        await RequestUtil.get(``,{}).then(()=>{
+                            message.success('确认成功！');
+                            setVisible(false);
+                        }).then(()=>{
+                            history.push(`/workshopManagement/processingTask`)
+                        })
+                    }} disabled={!(userDataSource.length>0)}>确认加工完成</Button>
+                </Space> } 
+                onCancel={ () => setVisible(false) }
+            >
+                <WorkshopUserSelectionComponent onSelect={ (selectedRows: object[] | any) => {
+                    let temp = [...userDataSource];    
+  
+                    temp.push(selectedRows[0]);
+                    setUserDataSource(temp);
+                    } } buttonTitle="添加员工"/>
+                <Table 
+                    columns={[
+                        { title: '姓名', dataIndex: 'name', key:'name' },
+                        { title: '操作', dataIndex: 'operation', key: 'operation' ,render: (_: undefined, record: Record<string, any>, index: number): React.ReactNode => (
+                            <Space direction="horizontal" size="small">
+                                <Popconfirm
+                                    title="确认删除?"
+                                    onConfirm={ () => delRow(index) }
+                                    okText="确认"
+                                    cancelText="取消"
+                                >
+                                    <Button type="link">删除</Button>
+                                </Popconfirm>
+                            </Space>
+                        ) }
+                    ]}
+                    dataSource={[...userDataSource]} 
+                    pagination={false}
+                />
+            </Modal>
+        </Spin>
+    </>
+}
