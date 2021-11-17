f82fdfeb4a8a78bece46b87da81f466883848f82
lixy <826446332@qq.com>
2021-11-17 11:58:29 +0800

    【开发】包装车间

--- src/pages/package-workshop/processingTask/ProcessingTaskList.tsx
+++ src/pages/package-workshop/processingTask/ProcessingTaskList.tsx
@@ -1,162 +1,70 @@
 import React, { useState } from 'react';
-import { Space, Input, DatePicker, Select } from 'antd';
+import { Space, Input, DatePicker, Select, Button, Form, TablePaginationConfig, Card } from 'antd';
 import { Page } from '../../common';
 import { FixedType } from 'rc-table/lib/interface';
-import { Link } from 'react-router-dom';
+import { Link, useHistory, useParams } from 'react-router-dom';
+import GantChart from '../../../components/GantChart';
+import { useForm } from 'antd/lib/form/Form';
+import RequestUtil from '../../../utils/RequestUtil';
+import useRequest from '@ahooksjs/use-request';
+import styles from '../workshop.module.less';
 
 export default function ProcessingTaskList(): React.ReactNode {
-    const [filterValue, setFilterValue] = useState({});
+    const [ refresh, setRefresh ] = useState<boolean>(false);
+    const [ filterValue, setFilterValue ] = useState({});
+    const history = useHistory();
+    const [ form ] = useForm();
+    const [ paragraphData, setParagraphData ] = useState([] as undefined | any);
+    const params = useParams<{ id: string ,productSegmentId: string,status:string}>();
+    const page = {
+        current: 1,
+        pageSize: 10
+    };
 
-    const columns = [
-        {
-            key: 'index',
-            title: '序号',
-            dataIndex: 'index',
-            width: 50,
-            render: (_: undefined, record: Record<string, any>, index: number): React.ReactNode => (<span>{index + 1}</span>)
-        },
-        {
-            key: 'name',
-            title: '塔型',
-            width: 150,
-            dataIndex: 'name'
-        },
-        {
-            key: 'steelProductShape',
-            title: '塔型钢印号',
-            dataIndex: 'steelProductShape',
-            width: 120
-        },
-        {
-            key: 'pattern',
-            title: '类型',
-            width: 200,
-            dataIndex: 'pattern',
-            render: (status: number): React.ReactNode => {
-                switch (status) {
-                    case 1:
-                        return '新放';
-                    case 2:
-                        return '重新出卡';
-                    case 3:
-                        return '套用';
-                }
-                return <>{status}</>
-            }
-        },
-        {
-            key: 'taskCode',
-            title: '任务单编号',
-            width: 150,
-            dataIndex: 'taskCode'
-        },
-        {
-            key: 'saleOrderNumber',
-            title: '订单编号',
-            dataIndex: 'saleOrderNumber',
-            width: 200
-        },
-        {
-            key: 'internalNumber',
-            title: '内部合同编号',
-            width: 200,
-            dataIndex: 'internalNumber'
-        },
-        {
-            key: 'structureCount',
-            title: '件号数',
-            width: 200,
-            dataIndex: 'structureCount',
-            render: (_: number): React.ReactNode => (
-                <span>{_ === -1 ? undefined : _}</span>
-            )
-        },
-        {
-            key: 'steelAngleCount',
-            title: '角钢件号数',
-            width: 200,
-            dataIndex: 'steelAngleCount',
-            render: (_: number): React.ReactNode => (
-                <span>{_ === -1 ? undefined : _}</span>
-            )
-        },
-        {
-            key: 'steelPlateCount',
-            title: '钢板件号数',
-            width: 200,
-            dataIndex: 'steelPlateCount',
-            render: (_: number): React.ReactNode => (
-                <span>{_ === -1 ? undefined : _}</span>
-            )
-        },
-        {
-            key: 'createTime',
-            title: '创建时间',
-            width: 200,
-            dataIndex: 'createTime'
-        },
-        {
-            key: 'updateUserName',
-            title: '最后更新人',
-            width: 200,
-            dataIndex: 'updateUserName'
-        },
-        {
-            key: 'updateTime',
-            title: '最后更新时间',
-            width: 200,
-            dataIndex: 'updateTime'
-        },
-        {
-            key: 'operation',
-            title: '操作',
-            dataIndex: 'operation',
-            fixed: 'right' as FixedType,
-            width: 200,
-            render: (_: undefined, record: Record<string, any>): React.ReactNode => (
-                <Space direction="horizontal" size="small">
-                    <Link to={`/drawTower/drawTowerMngt/towerInformation/${record.id}`}>塔型信息</Link>
-                    <Link to={`/drawTower/drawTowerMngt/componentInformation/${record.id}/${record.structureCount === -1 ? 0 : record.structureCount}`}>塔型构件</Link>
-                </Space>
-            )
-        }
-    ]
+    const getTableDataSource = (pagination: TablePaginationConfig, filterValues: Record<string, any>) => new Promise(async (resole, reject) => {
+        const data = await RequestUtil.get<any>(`/tower-science/productCategory`, { ...pagination, ...filterValues });
+        // setDetailData(data);
+        resole(data);
+    });
+    const { loading } = useRequest<any>(() => getTableDataSource(page, {}), {});
+    const onFinish = (value: Record<string, any>) => {
+        getTableDataSource(page, value);
+    }
 
-    return <Page
-        path="/tower-science/productCategory/draw/page"
-        columns={columns}
-        headTabs={[]}
-        searchFormItems={[
-            {
-                name: 'pattern',
-                label: '类型',
-                children: <Select style={{ width: '120px' }} placeholder="请选择">
-                    <Select.Option value={""} key="4">全部</Select.Option>
-                    <Select.Option value={1} key="1">新放</Select.Option>
-                    <Select.Option value={2} key="2">重新出卡</Select.Option>
-                    <Select.Option value={3} key="3">套用</Select.Option>
+    return <>
+    <Card style={{marginBottom:'8px'}}>
+        <Form form={ form } onFinish={ onFinish } layout="inline" className={ styles.topForm }>
+            <Form.Item name="status" label="工序">
+                <Select style={{width:'100px'}}>
+                    <Select.Option value={''} key ={''}>全部</Select.Option>
+                    <Select.Option value={1} key={1}>流水线</Select.Option>
                 </Select>
-            },
-            {
-                name: 'time',
-                label: '创建时间',
-                children: <DatePicker.RangePicker />
-            },
-            {
-                name: 'fuzzyMsg',
-                label: '模糊查询项',
-                children: <Input placeholder="塔型/塔型钢印号任务单编号/订单编号/内部合同编号· 0" />
-            }
-        ]}
-        filterValue={filterValue}
-        onFilterSubmit={(values: Record<string, any>) => {
-            if (values.time) {
-                const formatDate = values.time.map((item: any) => item.format("YYYY-MM-DD"));
-                values.creationTimeStart = formatDate[0] + ' 00:00:00';
-                values.creationTimeEnd = formatDate[1] + ' 23:59:59';
-            }
-            setFilterValue(values);
-            return values;
-        }}
-    />
+            </Form.Item>
+            <Form.Item name="status" label="产线">
+                <Select style={{width:'100px'}}>
+                    <Select.Option value={''} key ={''}>全部</Select.Option>
+                    <Select.Option value={1} key={1}>A产线</Select.Option>
+                    <Select.Option value={2} key={2}>B产线</Select.Option>
+                </Select>
+            </Form.Item>
+            <Form.Item name="time" label="时间范围">
+                <DatePicker.RangePicker/>
+            </Form.Item>
+            {/* <Form.Item name="productCategoryName" label="模糊查询项">
+                <Input placeholder="请输入塔型/构件号进行查询"/>
+            </Form.Item> */}
+            <Space direction="horizontal">
+                <Button type="primary" htmlType="submit">搜索</Button>
+                <Button type="ghost" htmlType="reset">重置</Button>
+            </Space>
+        </Form>
+    </Card>
+    <Card>
+        <Button type='primary' onClick={()=>{history.push(`/packagingWorkshop/processingTask/dispatch/new`)}}>派工</Button>
+        <GantChart value={[{
+            id:'1',deptProcessesName:'流水线', name:'设备A',startTime:new Date().getTime(), endTime:new Date('2021-11-20 14:53:22').getTime()
+        },{
+            id:'2',deptProcessesName:'流水线', name:'设备B',startTime:new Date('2021-11-20 2:40:22').getTime(), endTime:new Date('2021-11-20 6:10:00').getTime()
+        }]} /></Card>
+    </>
 }
