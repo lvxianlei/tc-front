b1d4f6ff1b2f868e7e6f99615c86afc3ea15e625
zhanghua <18231209794@163.com>
2021-11-24 18:52:48 +0800

    弹框ui

--- src/pages/workMngt/bolt/BoltDetail.tsx
+++ src/pages/workMngt/bolt/BoltDetail.tsx
@@ -1,8 +1,9 @@
-import { Button } from 'antd';
+import { Button, Modal, Input, Select, Table } from 'antd';
 import React, { useState } from 'react';
 import { useHistory, useRouteMatch } from 'react-router-dom';
 import RequestUtil from '../../../utils/RequestUtil';
 import { Page } from '../../common';
+import './BoltDetailList.less';
 
 export default function BoltCheck(): React.ReactNode {
     const match: any = useRouteMatch()
@@ -82,6 +83,52 @@ export default function BoltCheck(): React.ReactNode {
             }
         },
     ]
+    // 提交问题单表头
+    const columnsTow = [
+        {
+            title: '序号',
+            dataIndex: 'index',
+            width: 100,
+            render: (_: undefined, record: Record<string, any>, index: number): React.ReactNode => {
+                return <span>{index + 1}</span>
+            }
+        },
+        {
+            title: '操作部门',
+            width: 150,
+            dataIndex: 'typeName',
+        },
+        {
+            title: '操作人',
+            width: 150,
+            dataIndex: 'name',
+        },
+        {
+            title: '操作时间',
+            width: 150,
+            dataIndex: 'level',
+        },
+        {
+            title: '问题单状态',
+            width: 150,
+            dataIndex: 'specs',
+        },
+        {
+            title: '备注',
+            dataIndex: 'unbuckleLength',
+            width: 120,
+        },
+    ]
+    const [visible, setVisible] = useState<boolean>(false);//添加弹框显示
+    const [problemVisible, setProblemVisible] = useState<boolean>(false);//提交问题弹框显示
+    // 弹框取消
+    const onCancel = () => {
+        setVisible(false)
+    }
+    // 螺栓信息添加
+    const onSubmit = () => {
+
+    }
     return (
         <div>
             <Page
@@ -92,13 +139,286 @@ export default function BoltCheck(): React.ReactNode {
                         <Button type="primary" ghost>模板下载</Button>
                         <Button type="primary" ghost onClick={() => { }} style={{ marginLeft: 10, }}>编辑/锁定</Button>
                         <Button type="primary" ghost onClick={() => { }} style={{ marginLeft: 10, }}>导入</Button>
-                        <Button type="primary" ghost onClick={() => { }} style={{ marginLeft: 10, }}>添加</Button>
+                        <Button type="primary" ghost onClick={() => { setVisible(true) }} style={{ marginLeft: 10, }}>添加</Button>
                         <Button type="primary" ghost onClick={() => { history.go(-1) }} style={{ marginLeft: 10, }}>返回上一级</Button>
                     </div>
                 }
                 headTabs={[]}
                 searchFormItems={[]}
             />
+            {/* 螺栓添加 */}
+            <Modal
+                className='Modal_hugao'
+                visible={visible}
+                width="50%"
+                title="添加"
+                onCancel={() => { onCancel(); }}
+                onOk={() => onSubmit()}
+                okText="确定"
+                cancelText="关闭"
+            >
+                <div className="add_HuGao">
+                    <div className="tr">
+                        <div className="td">
+                            <div className="title">螺栓类型*</div>
+                            <div className="val">
+                                <Select
+                                    className='input'
+                                    style={{ width: '100%' }}
+                                    // mode="multiple"
+                                    maxLength={3}
+                                    // value={towerTagNo}
+                                    onChange={(value) => {
+                                    }}
+                                >
+                                    {/* {
+                                        towerTagList.map((item: any, index: any) => {
+                                            return <Select.Option value={item.id} key={index}>{item.productNumber}</Select.Option>
+                                        })
+                                    } */}
+
+                                    <Select.Option value={155} key={155}>螺栓类型1</Select.Option>
+                                    <Select.Option value={152} key={152}>螺栓类型2</Select.Option>
+                                    <Select.Option value={154} key={154}>螺栓类型3</Select.Option>
+                                </Select>
+                            </div>
+                        </div>
+                        <div className="td">
+                            <div className="title">名称*</div>
+                            <div className="val">
+                                <Input
+                                    className="input"
+                                    placeholder="请输入"
+                                    bordered={false}
+                                    maxLength={10}
+                                    // value={callHeight}
+                                    onChange={(e) => {
+                                        // setCallHeight(e.target.value.replace(/\D/g, ''))
+                                    }}
+                                ></Input>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="tr">
+                        <div className="td">
+                            <div className="title">规格*</div>
+                            <div className="val">
+                                <Input
+                                    className="input"
+                                    placeholder="请输入"
+                                    bordered={false}
+                                    maxLength={20}
+                                    // value={callHeight}
+                                    onChange={(e) => {
+                                        // setCallHeight(e.target.value.replace(/\D/g, ''))
+                                    }}
+                                ></Input>
+                            </div>
+                        </div>
+                        <div className="td">
+                            <div className="title">无扣长</div>
+                            <div className="val">
+                                <Input
+                                    className="input"
+                                    placeholder="请输入"
+                                    bordered={false}
+                                    maxLength={3}
+                                    // value={callHeight}
+                                    onChange={(e) => {
+                                        // setCallHeight(e.target.value.replace(/\D/g, ''))
+                                    }}
+                                ></Input>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="tr">
+                        <div className="td">
+                            <div className="title">等级*</div>
+                            <div className="val">
+                                <Input
+                                    className="input"
+                                    placeholder="请输入"
+                                    bordered={false}
+                                    maxLength={20}
+                                    // value={callHeight}
+                                    onChange={(e) => {
+                                        // setCallHeight(e.target.value.replace(/\D/g, ''))
+                                    }}
+                                ></Input>
+                            </div>
+                        </div>
+                        <div className="td">
+                            <div className="title">单重*</div>
+                            <div className="val">
+                                <Input
+                                    className="input"
+                                    placeholder="请输入"
+                                    bordered={false}
+                                    maxLength={4}
+                                    // value={callHeight}
+                                    onChange={(e) => {
+                                        // setCallHeight(e.target.value.replace(/\D/g, ''))
+                                    }}
+                                ></Input>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="tr">
+                        <div className="td">
+                            <div className="title">小计*</div>
+                            <div className="val">
+                                <Input
+                                    className="input"
+                                    placeholder="请输入"
+                                    bordered={false}
+                                    maxLength={4}
+                                    // value={callHeight}
+                                    onChange={(e) => {
+                                        // setCallHeight(e.target.value.replace(/\D/g, ''))
+                                    }}
+                                ></Input>
+                            </div>
+                        </div>
+                        <div className="td">
+                            <div className="title">合计*</div>
+                            <div className="val">
+                                <Input
+                                    className="input"
+                                    placeholder="请输入"
+                                    bordered={false}
+                                    maxLength={4}
+                                    // value={callHeight}
+                                    onChange={(e) => {
+                                        // setCallHeight(e.target.value.replace(/\D/g, ''))
+                                    }}
+                                ></Input>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="tr">
+                        <div className="td">
+                            <div className="title">总重(kg)*</div>
+                            <div className="val">
+                                <Input
+                                    className="input"
+                                    placeholder="自动计算"
+                                    bordered={false}
+                                    disabled
+                                ></Input>
+                            </div>
+                        </div>
+                        <div className="td">
+                            <div className="title">备注*</div>
+                            <div className="val">
+                                <Input
+                                    className="input"
+                                    placeholder="请输入"
+                                    bordered={false}
+                                    maxLength={100}
+                                    // value={callHeight}
+                                    onChange={(e) => {
+                                        // setCallHeight(e.target.value.replace(/\D/g, ''))
+                                    }}
+                                ></Input>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Modal>
+            {/* 提交问题 */}
+            <Modal
+                className='Modal_hugao'
+                visible={problemVisible}
+                width="60%"
+                title="提交问题"
+                onCancel={() => { onCancel(); }}
+                onOk={() => onSubmit()}
+                okText="提交问题"
+                cancelText="关闭"
+            >
+                <div className='title_ON'>
+                    问题信息
+                </div>
+                <div className="add_HuGao">
+                    <div className="tr">
+                        <div className="td">
+                            <div className="title">问题字段*</div>
+                            <div className="val">
+                                <Input
+                                    className="input"
+                                    placeholder="请输入"
+                                    bordered={false}
+                                    maxLength={20}
+                                    // value={callHeight}
+                                    onChange={(e) => {
+                                        // setCallHeight(e.target.value.replace(/\D/g, ''))
+                                    }}
+                                ></Input>
+                            </div>
+                        </div>
+                        <div className="td">
+                            <div className="title">原字段信息</div>
+                            <div className="val">
+                                <Input
+                                    className="input"
+                                    placeholder="请输入"
+                                    bordered={false}
+                                    maxLength={3}
+                                    // value={callHeight}
+                                    onChange={(e) => {
+                                        // setCallHeight(e.target.value.replace(/\D/g, ''))
+                                    }}
+                                ></Input>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="tr">
+                        <div className="td">
+                            <div className="title">备注</div>
+                            <div className="val">
+                                <Input
+                                    className="input"
+                                    placeholder="请输入"
+                                    bordered={false}
+                                    maxLength={10}
+                                    // value={callHeight}
+                                    onChange={(e) => {
+                                        // setCallHeight(e.target.value.replace(/\D/g, ''))
+                                    }}
+                                ></Input>
+                            </div>
+                        </div>
+                        <div className="td">
+                            <div className="title">校对后信息*</div>
+                            <div className="val">
+                                <Input
+                                    className="input"
+                                    placeholder="请输入"
+                                    bordered={false}
+                                    maxLength={10}
+                                    // value={callHeight}
+                                    onChange={(e) => {
+                                        // setCallHeight(e.target.value.replace(/\D/g, ''))
+                                    }}
+                                ></Input>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='title_ON'>
+                    操作信息
+                </div>
+                <Table
+                    scroll={{ x: true }}
+                    className='public_table'
+                    columns={columnsTow}
+                    // dataSource={columnsData}
+                    pagination={false}
+                    size='small'
+                >
+
+                </Table>
+            </Modal>
         </div>
     )
 }
--- src/pages/workMngt/bolt/BoltDetailList.tsx
+++ src/pages/workMngt/bolt/BoltDetailList.tsx
@@ -1,8 +1,10 @@
-import { Button } from 'antd';
+import { Button, Input, message, Modal, Select } from 'antd';
+import TextArea from 'rc-textarea';
 import React, { useState } from 'react';
 import { useHistory, useRouteMatch } from 'react-router-dom';
 import RequestUtil from '../../../utils/RequestUtil';
 import { Page } from '../../common';
+import './BoltDetailList.less';
 
 export default function BoltCheck(): React.ReactNode {
     const match: any = useRouteMatch()
@@ -45,7 +47,7 @@ export default function BoltCheck(): React.ReactNode {
             title: '操作',
             width: 120,
             dataIndex: 'operation',
-            render: () => {
+            render: (text: any, item: any,) => {
                 return (
                     <div className='operation'>
                         <span
@@ -60,7 +62,7 @@ export default function BoltCheck(): React.ReactNode {
                         >编辑</span>
                         <span
                             onClick={() => {
-                                history.push('')
+                                history.push(`/workMngt/boltList/boltListing/${match.params.id}/${item.id}`)
                             }}
                         >螺栓明细</span>
                     </div>
@@ -68,6 +70,13 @@ export default function BoltCheck(): React.ReactNode {
             }
         },
     ]
+    const [visible, setVisible] = useState<boolean>(false);//添加弹框显示
+    const [callHeight, setCallHeight] = useState<number | string>('');//呼高
+    const [towerTagNo, setTowerTagNo] = useState<any>([]);//塔位号
+    const [towerTagList, setTowerTagList] = useState<any>([]);//塔位列表
+    const [explain, setExplain] = useState<any>('');//说明
+    const [refresh, setRefresh] = useState(false);
+    console.log(match, 'match')
     /**
      * 完成
      */
@@ -75,22 +84,126 @@ export default function BoltCheck(): React.ReactNode {
         await RequestUtil.put('/tower-science/boltRecord/completeCheck', {
         })
     }
+    // 添加
+    const onSubmit = async () => {
+        await RequestUtil.post('/tower-science/boltRecord/saveBasicHeight', {
+            basicHeight: callHeight,
+            productIdList: towerTagNo,
+            description: explain,
+            productCategoryId: match.params.id
+        })
+        setRefresh(!refresh)
+    }
+    // 获取塔位号
+    const getBoltlist = async () => {
+        let ary = await RequestUtil.get('/tower-science/product/listByBolt', {
+            productCategoryId: match.params.id
+        })
+        setTowerTagList(ary)
+    }
+    // 弹框取消
+    const onCancel = () => {
+        setVisible(false)
+    }
     return (
         <div>
             <Page
                 path={`/tower-science/boltRecord/basicHeight/${match.params.id}`}
                 columns={columns}
+                refresh={refresh}
                 extraOperation={
                     <div>
                         <Button type="primary" ghost>导出</Button>
                         <Button type="primary" ghost onClick={() => { successCheck() }} style={{ marginLeft: 10, }}>完成</Button>
-                        <Button type="primary" ghost onClick={() => { successCheck() }} style={{ marginLeft: 10, }}>添加</Button>
+                        <Button type="primary" ghost onClick={() => { setVisible(true); getBoltlist() }} style={{ marginLeft: 10, }}>添加</Button>
                         <Button type="primary" ghost onClick={() => { history.go(-1) }} style={{ marginLeft: 10, }}>返回上一级</Button>
                     </div>
                 }
                 headTabs={[]}
                 searchFormItems={[]}
             />
+            <Modal
+                className='Modal_hugao'
+                visible={visible}
+                width="50%"
+                title="添加"
+                onCancel={() => { onCancel(); }}
+                onOk={() => onSubmit()}
+                okText="确定"
+                cancelText="关闭"
+            >
+                <div className="add_HuGao">
+                    <div className="tr">
+                        <div className="td">
+                            <div className="title">呼高</div>
+                            <div className="val">
+                                <Input
+                                    className="input"
+                                    placeholder="请输入"
+                                    bordered={false}
+                                    maxLength={3}
+                                    value={callHeight}
+                                    onChange={(e) => {
+                                        setCallHeight(e.target.value.replace(/\D/g, ''))
+                                    }}
+                                ></Input>
+                            </div>
+                        </div>
+                        <div className="td">
+                            <div className="title">塔位号</div>
+                            <div className="val">
+                                <Select
+                                    className='input'
+                                    style={{ width: '100%' }}
+                                    mode="multiple"
+                                    maxLength={3}
+                                    value={towerTagNo}
+                                    onChange={(value) => {
+                                        if (value.length > 99) {
+                                            message.error('塔位号最多99个')
+                                            return
+                                        }
+                                        console.log(value)
+                                        setTowerTagNo(value)
+                                    }}
+                                >
+                                    {
+                                        towerTagList.map((item: any, index: any) => {
+                                            return <Select.Option value={item.id} key={index}>{item.productNumber}</Select.Option>
+                                        })
+                                    }
+
+                                    <Select.Option value={155} key={155}>塔位1</Select.Option>
+                                    <Select.Option value={152} key={152}>塔位2</Select.Option>
+                                    <Select.Option value={154} key={154}>塔位3</Select.Option>
+                                </Select>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="tr">
+                        <div className="td">
+                            <div className="title">说明</div>
+                            <div className="val">
+                                <TextArea
+                                    className="TextArea"
+                                    placeholder="请输入"
+                                    style={{ width: '100%' }}
+                                    maxLength={400}
+                                    value={explain}
+                                    onChange={(e) => {
+                                        setExplain(e.target.value)
+                                    }}
+                                ></TextArea>
+                            </div>
+                        </div>
+                        <div className="td">
+                            <div className="title"></div>
+                            <div className="val">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Modal>
         </div>
     )
 }
