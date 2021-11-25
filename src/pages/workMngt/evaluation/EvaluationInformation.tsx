b78ec51ada01b83f4a41c13d4b7edf3f887681a2
zyc <757715809@qq.com>
2021-11-25 11:56:08 +0800

    【开发】RD迭代-评估列表-上传附件调整

--- src/pages/workMngt/evaluation/EvaluationInformation.tsx
+++ src/pages/workMngt/evaluation/EvaluationInformation.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Button, Space, Modal, Input, Upload, Form, FormInstance, message, Image } from 'antd';
-import { DetailContent, CommonTable, Attachment } from '../../common';
+import { Button, Space, Modal, Input, Form, FormInstance } from 'antd';
+import { DetailContent, Attachment } from '../../common';
 import RequestUtil from '../../../utils/RequestUtil';
 import styles from './Evaluation.module.less';
 import { WithTranslation, withTranslation } from 'react-i18next';
 import { RouteComponentProps, withRouter } from 'react-router';
-import AuthUtil from '../../../utils/AuthUtil';
 import { FileProps } from '../../common/Attachment';
 
 interface IResponse {
@@ -124,39 +123,7 @@ class EvaluationInformation extends React.Component<IEvaluationInformationRouteP
             >
                 <Form onFinish={() => this.onSave()} ref={this.form}>
                     <DetailContent>
-                        {/* <p>说明文件</p> */}
-                        <Attachment title="说明文件" dataSource={this.state.information?.instructionFileList || []} />
-                        {/* <CommonTable 
-                            columns={[
-                                { 
-                                    key: 'name', 
-                                    title: '附件', 
-                                    dataIndex: 'name',
-                                    width: 250 
-                                },
-                                { 
-                                    key: 'operation', 
-                                    title: '操作', 
-                                    dataIndex: 'operation', 
-                                    render: (_: undefined, record: Record<string, any>): React.ReactNode => (
-                                        <Space direction="horizontal" size="small">
-                                            <Button type="link" onClick={ () => window.open(record.filePath) }>下载</Button>
-                                            {
-                                                record.fileSuffix === 'pdf' 
-                                                ? 
-                                                <Button type="link" onClick={ () => window.open(record.filePath) }>预览</Button> 
-                                                : ['jpg', 'jpeg', 'png', 'gif'].includes(record.fileSuffix) 
-                                                ? 
-                                                <Button type='link' onClick={ () => { this.setState({ pictureUrl: record.filePath, pictureVisible: true }) } }>预览</Button> 
-                                                : null 
-                                            }
-                                        </Space>
-                                    )
-                                }
-                            ]}
-                            dataSource={ this.state.information?.instructionFileList }
-                            pagination={ false }
-                        /> */}
+                        <Attachment title="说明文件" dataSource={ this.state.information?.instructionFileList } />
                         <p className={styles.topPadding}>评估信息<span style={{ color: 'red' }}>*</span></p>
                         <Form.Item
                             name="description"
@@ -166,109 +133,9 @@ class EvaluationInformation extends React.Component<IEvaluationInformationRouteP
                             <Input.TextArea placeholder="请输入" maxLength={300} disabled={this.state.information?.status === 4} showCount />
                         </Form.Item>
                         <Attachment title="评估文件" edit={!(this.state.information?.status === 4)} dataSource={this.state.information?.assessFileList || []} />
-                        {/* <p className={ styles.topPadding }>评估文件
-                        <span style={ { position: 'absolute', right: '1%' } }>
-                            {
-                                this.state.information?.status === 4 ? null :
-                                <Upload 
-                                    action={ () => {
-                                        const baseUrl: string | undefined = process.env.REQUEST_API_PATH_PREFIX;
-                                        return baseUrl+'/sinzetech-resource/oss/put-file'
-                                    } } 
-                                    headers={
-                                        {
-                                            'Authorization': `Basic ${ AuthUtil.getAuthorization() }`,
-                                            'Tenant-Id': AuthUtil.getTenantId(),
-                                            'Sinzetech-Auth': AuthUtil.getSinzetechAuth()
-                                        }
-                                    }
-                                    showUploadList={ false }
-                                    data={ { productCategoryId: this.props.id } }
-                                    onChange={ (info) => {
-                                        if(info.file.response && !info.file.response?.success) {
-                                            message.warning(info.file.response?.msg)
-                                        } 
-                                        if(info.file.response && info.file.response?.success) {
-                                            let resData: FileProps = info.file.response?.data;
-                                            this.setState({
-                                                information: {
-                                                    ...this.state.information,
-                                                    assessFileList: [
-                                                        ...this.state.information?.assessFileList || [],
-                                                        { 
-                                                            link: resData.link,
-                                                            filePath: resData.name,
-                                                            name: resData.originalName,
-                                                            userName: resData.userName,
-                                                            fileSuffix: resData.originalName?.split('.')[resData.originalName?.split('.').length - 1]
-                                                        }
-                                                    ]
-                                                }
-                                            })
-                                        }
-                                    }}>
-                                    <Button type='primary' ghost>添加</Button>
-                                </Upload>
-                            }
-                            </span> 
-                        </p> */}
-                        {/* <CommonTable    
-                            columns={[
-                                { 
-                                    key: 'name', 
-                                    title: '附件', 
-                                    dataIndex: 'name',
-                                    width: 250
-                                },
-                                { 
-                                    key: 'operation', 
-                                    title: '操作', 
-                                    dataIndex: 'operation', 
-                                    render: (_: undefined, record: Record<string, any>, index: number): React.ReactNode => (
-                                        <Space direction="horizontal" size="small">
-                                            {
-                                                record.id ? <Button type="link" onClick={ () => window.open(record.id ? record.filePath : record.link) }>下载</Button> : <Button type="link" onClick={ () => window.open(record.id ? record.filePath : record.link) }>下载</Button> 
-                                            }
-                                            {
-                                                record.fileSuffix === 'pdf' 
-                                                ? 
-                                                <Button type="link" onClick={ () => window.open(record.id ? record.filePath : record.link) }>预览</Button> 
-                                                : 
-                                                ['jpg', 'jpeg', 'png', 'gif'].includes(record.fileSuffix) 
-                                                ? 
-                                                <Button type='link' onClick={ () => { this.setState({ pictureUrl: record.id ? record.filePath : record.link, pictureVisible: true }) } }>预览</Button> 
-                                                : null 
-                                            }
-                                            {
-                                                this.state.information?.status === 4 ? null :
-                                                <Button type="link" onClick={ () => {
-                                                    const information: IResponse = this.state.information || {};
-                                                    information.assessFileList && information.assessFileList.splice(index, 1);
-                                                    this.setState({
-                                                        information: {
-                                                            ...information,
-                                                            assessFileList: [ ...information.assessFileList || [] ]
-                                                        }
-                                                    })
-                                                } }>删除</Button>
-                                            }
-                                        </Space>
-                                    ) 
-                                }
-                            ]}
-                            dataSource={ this.state.information?.assessFileList }
-                            pagination={ false }
-                        /> */}
                     </DetailContent>
                 </Form>
             </Modal>
-            {/* <Modal visible={ this.state.pictureVisible } onCancel={ () => {
-                this.setState({
-                    pictureVisible: false
-                })
-            } } footer={ false }>
-                <Image src={ this.state.pictureUrl } preview={ false } />
-            </Modal> */}
         </>
     }
 }
