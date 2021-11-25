50e9393acea6d3c81e8889e75b84dfe2cdf2873b
一片叶子 <944832260@qq.com>
2021-11-25 15:56:49 +0800

    螺栓清单导入

--- src/pages/workMngt/bolt/BoltDetail.tsx
+++ src/pages/workMngt/bolt/BoltDetail.tsx
@@ -1,7 +1,9 @@
-import { Button, } from 'antd';
+import { Button, message, Modal, Upload, } from 'antd';
 import React, { useState } from 'react';
 import { useHistory, useParams, } from 'react-router-dom';
+import AuthUtil from '../../../utils/AuthUtil';
 import { Page } from '../../common';
+import { downloadTemplate } from '../setOut/downloadTemplate';
 import BoltDetailAdd from './addModal';
 import './BoltDetailList.less';
 import BoltDetailProblem from './problemModal';
@@ -9,6 +11,8 @@ import BoltDetailProblem from './problemModal';
 export default function BoltCheck(): React.ReactNode {
     const params = useParams<{ id: string, boltId: string }>();
     const history = useHistory()
+    const [urlVisible, setUrlVisible] = useState<boolean>(false);
+    const [url, setUrl] = useState<string>('');
     const columns: any = [
         {
             title: '序号',
@@ -114,14 +118,44 @@ export default function BoltCheck(): React.ReactNode {
     return (
         <div>
             <Page
-                path={`/tower-science/boltRecord/basicHeight/${params.id}`}
+                path={`/tower-science/boltRecord/basicHeight/${params.boltId}`}
                 columns={columns}
                 refresh={refresh}
                 extraOperation={
                     <div>
-                        <Button type="primary" ghost>模板下载</Button>
+                        <Button type="primary" onClick={() => downloadTemplate('/tower-science/boltRecord/exportTemplate', '螺栓导入模板')} ghost>模板下载</Button>
                         <Button type="primary" ghost onClick={() => { }} style={{ marginLeft: 10, }}>编辑/锁定</Button>
-                        <Button type="primary" ghost onClick={() => { }} style={{ marginLeft: 10, }}>导入</Button>
+                        <Upload
+                            action={() => {
+                                const baseUrl: string | undefined = process.env.REQUEST_API_PATH_PREFIX;
+                                return baseUrl + '/tower-science/boltRecord/import'
+                            }}
+                            headers={
+                                {
+                                    'Authorization': `Basic ${AuthUtil.getAuthorization()}`,
+                                    'Tenant-Id': AuthUtil.getTenantId(),
+                                    'Sinzetech-Auth': AuthUtil.getSinzetechAuth()
+                                }
+                            }
+                            data={{ basicHeightId: params.id, productCategoryId: params.boltId }}
+                            showUploadList={false}
+                            onChange={(info) => {
+                                if (info.file.response && !info.file.response?.success) {
+                                    message.warning(info.file.response?.msg)
+                                }
+                                if (info.file.response && info.file.response?.success) {
+                                    if (Object.keys(info.file.response?.data).length > 0) {
+                                        setUrl(info.file.response?.data);
+                                        setUrlVisible(true);
+                                    } else {
+                                        message.success('导入成功！');
+                                        setRefresh(!refresh)
+                                    }
+                                }
+                            }}
+                        >
+                            <Button type="primary" ghost onClick={() => { }} style={{ marginLeft: 10, }}>导入</Button>
+                        </Upload>
                         <Button type="primary" ghost onClick={() => { setIsProblemModal(true) }} style={{ marginLeft: 10, }}>添加</Button>
                         <Button type="primary" ghost onClick={() => { history.go(-1) }} style={{ marginLeft: 10, }}>返回上一级</Button>
                     </div>
@@ -129,6 +163,18 @@ export default function BoltCheck(): React.ReactNode {
                 headTabs={[]}
                 searchFormItems={[]}
             />
+            <Modal
+                visible={urlVisible}
+                onOk={() => {
+                    window.open(url);
+                    setUrlVisible(false);
+                }}
+                onCancel={() => { setUrlVisible(false); setUrl('') }}
+                title='提示'
+                okText='下载'
+            >
+                当前存在错误数据，请重新下载上传！
+            </Modal>
             {
                 isAddModal ?
                     <BoltDetailAdd
