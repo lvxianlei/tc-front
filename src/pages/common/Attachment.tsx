393aac6de4c4ce997f294990a65a36d535f3fdf1
lvxianlei <lvxianlei@hotmail.com>
2021-11-27 18:08:23 +0800

    附件完善，暴露onDoneChange方法用以在上传成功后调用

--- src/pages/common/Attachment.tsx
+++ src/pages/common/Attachment.tsx
@@ -1,20 +1,20 @@
 import React, { useState, useImperativeHandle, forwardRef, useCallback, useEffect, ReactNode } from 'react'
-import { Button, Upload, Modal, Image, message, List, Avatar } from 'antd'
-import { DetailTitle, CommonTable } from "../common"
-import AuthUtil from "../../utils/AuthUtil"
+import { Button, Upload, Modal, Image, message, Row, Col, Spin, Empty } from 'antd'
+import { DetailTitle } from "../common"
 import RequestUtil from "../../utils/RequestUtil"
 import useRequest from '@ahooksjs/use-request'
 import { downLoadFile } from "../../utils"
-import { UploadFile } from 'antd/lib/upload/interface'
+import styles from "./CommonTable.module.less"
 export interface FileProps {
     id?: string,
     uid?: number | string,
     originalName?: string,
     fileName?: string,
-    filePath: string,
-    fileSuffix: string
-    fileSize: string | number
-    downloadUrl: string
+    filePath?: string,
+    fileSuffix?: string
+    fileSize?: string | number
+    downloadUrl?: string
+    loading?: boolean
 }
 export interface Actions {
     download: (path: string) => void
@@ -67,7 +67,8 @@ interface URLProps {
     updateTime?: string
     updateUser?: string
     uploadTime?: string
-    uploadUser?: string
+    uploadUser?: string,
+    uid?: string
 }
 
 export default forwardRef(function ({
@@ -83,13 +84,13 @@ export default forwardRef(function ({
     onDoneChange = () => { }
 }: AttachmentProps, ref): JSX.Element {
     const inputAccepts = accept ? ({ accept }) : ({})
-    const [attchs, setAttachs] = useState<FileProps[]>(dataSource)
+    const [attchs, setAttachs] = useState<FileProps[]>(dataSource.map(item => ({ ...item, uid: item.id })))
     const [visible, setVisible] = useState<boolean>(false)
     const [uploadOSSUrlInfo, setUploadOSSUrlInfo] = useState<URLProps>({
         pushUrl: "http://www."
     })
     const [picUrl, setPicUrl] = useState<string>()
-    const { run: saveFile, data: filesData } = useRequest<URLProps>((data: any) => new Promise(async (resole, reject) => {
+    const { run: saveFile, loading } = useRequest<URLProps>((data: any) => new Promise(async (resole, reject) => {
         try {
             const result: URLProps = await RequestUtil.post(`/sinzetech-resource/oss/endpoint/get-upload-url`, data)
             resole(result)
@@ -104,6 +105,11 @@ export default forwardRef(function ({
 
     const handleBeforeUpload = useCallback((event: File): Promise<boolean> => new Promise(async (resove, reject) => {
         try {
+            setAttachs([...attchs, {
+                uid: (event as any).uid,
+                originalName: event.name || "",
+                loading: true
+            }])
             const result: URLProps = await saveFile({
                 fileName: event.name,
                 fileSize: event.size
@@ -113,12 +119,26 @@ export default forwardRef(function ({
         } catch (error) {
             reject(false)
         }
-    }), [])
+    }), [attchs, setAttachs, setUploadOSSUrlInfo])
 
     const uploadChange = useCallback((event: any) => {
         if (event.file.status === "done") {
             if (event.file.xhr.status === 200) {
-                setAttachs([...attchs, {
+                setAttachs(attchs.map(item => {
+                    if (item.uid === event.file.uid) {
+                        return ({
+                            ...item,
+                            loading: false,
+                            filePath: uploadOSSUrlInfo?.downloadUrl || "",
+                            originalName: uploadOSSUrlInfo?.originalName || "",
+                            fileSuffix: uploadOSSUrlInfo?.fileSuffix || "",
+                            fileSize: uploadOSSUrlInfo?.fileSize || "",
+                            downloadUrl: uploadOSSUrlInfo?.downloadUrl || ""
+                        })
+                    }
+                    return item
+                }))
+                onDoneChange([{
                     id: uploadOSSUrlInfo?.id || "",
                     uid: event.file.uid,
                     filePath: uploadOSSUrlInfo?.downloadUrl || "",
@@ -127,7 +147,6 @@ export default forwardRef(function ({
                     fileSize: uploadOSSUrlInfo?.fileSize || "",
                     downloadUrl: uploadOSSUrlInfo?.downloadUrl || ""
                 }])
-                onDoneChange(attchs)
             }
         }
     }, [setAttachs, attchs, setUploadOSSUrlInfo, onDoneChange, uploadOSSUrlInfo])
@@ -139,10 +158,10 @@ export default forwardRef(function ({
     useImperativeHandle(ref, () => ({ getDataSource, dataSource: attchs, resetFields }), [JSON.stringify(attchs), getDataSource, dataSource, resetFields])
 
     const handlePreview = useCallback((record: FileProps) => {
-        if (["png", "jpeg", "jpg", "gif"].includes(record.fileSuffix)) {
+        if (["png", "jpeg", "jpg", "gif"].includes(record?.fileSuffix || "")) {
             setPicUrl(record.filePath)
             setVisible(true)
-        } else if (["pdf"].includes(record.fileSuffix)) {
+        } else if (["pdf"].includes(record?.fileSuffix || "")) {
             window.open(record.filePath)
         } else {
             message.warning("暂只支持*.png,*.jpeg,*.jpg,*.gif*.pdf预览...")
@@ -151,7 +170,7 @@ export default forwardRef(function ({
 
     const handleCancel = useCallback(() => setVisible(false), [setVisible])
 
-    const operationRender = useCallback((value: any, records: any) => {
+    const operationRender = useCallback((records: any) => {
         if (renderActions) {
             return renderActions(records, {
                 preview: handlePreview,
@@ -160,9 +179,9 @@ export default forwardRef(function ({
             })
         }
         return <>
-            {!edit && <Button type="link" onClick={() => handlePreview(records)}>预览</Button>}
-            <Button type="link" onClick={() => downLoadFile(records.downloadUrl)}>下载</Button>
-            {edit && <Button type="link" onClick={() => deleteAttachData(records.id)}>删除</Button>}
+            {!edit && <Button size="small" type="link" onClick={() => handlePreview(records)}>预览</Button>}
+            <Button size="small" type="link" onClick={() => downLoadFile(records.downloadUrl)}>下载</Button>
+            {edit && <Button size="small" type="link" onClick={() => deleteAttachData(records.uid)}>删除</Button>}
         </>
     }, [])
 
@@ -212,13 +231,18 @@ export default forwardRef(function ({
         >
             {children}
         </Upload>}
-        {isTable && <CommonTable columns={[
-            { title: "文件名称", dataIndex: "originalName" },
-            {
-                title: "操作",
-                dataIndex: "operation",
-                render: operationRender
-            }
-        ]} dataSource={attchs} />}
+        {isTable && <div>
+            <Row style={{ backgroundColor: "#fafafa", height: 32, lineHeight: "32px" }}>
+                <Col span={12} style={{ padding: 8 }}>文件名称</Col>
+                <Col span={12} style={{ padding: 8 }}>操作</Col>
+            </Row>
+            {!attchs.length && <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />}
+            {attchs.map((item, index: number) => <Spin key={item.uid} spinning={item.loading} size="small">
+                <Row style={{ height: 28, lineHeight: "28px", backgroundColor: index % 2 === 0 ? "#fff" : "#f8f8f8" }} >
+                    <Col span={12} style={{ padding: "0 8px" }}>{item.originalName}</Col>
+                    <Col span={12} style={{ padding: "0 8px" }}>{operationRender(item)}</Col>
+                </Row>
+            </Spin>)}
+        </div>}
     </>
 })
