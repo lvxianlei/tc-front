bfebd3f74bd87b2d0c67fafe0596fbea3a7216cf
lvxianlei <lvxianlei@hotmail.com>
2021-12-01 20:56:09 +0800

    【防止注入】弹框中搜索框输入特殊符号的处理！

--- src/pages/common/FormItemType.tsx
+++ src/pages/common/FormItemType.tsx
@@ -5,6 +5,7 @@ import { PlusOutlined } from "@ant-design/icons"
 import RequestUtil from '../../utils/RequestUtil'
 import useRequest from '@ahooksjs/use-request'
 import moment from 'moment'
+import { stringify } from 'query-string';
 export type FormItemTypesType = "text" | "number" | "select" | "date" | "textarea" | "popForm" | undefined
 
 interface SelectOption {
@@ -82,9 +83,9 @@ export const PopTableContent: React.FC<{ data: PopTableData, value?: { id: strin
                     delete params[columnItem?.dataIndex]
                 }
             })
-            const paramsOptions = Object.keys(params).map((item: string) => `${item}=${params[item] || ""}`).join("&")
+            const paramsOptions = stringify(params)
             const path = data.path.includes("?") ? `${data.path}&${paramsOptions || ''}` : `${data.path}?${paramsOptions || ''}`
-            resolve(await RequestUtil.get<{ data: any }>(path))
+            resolve(await RequestUtil.get<{ data: any }>(JSON.stringify(path)))
         } catch (error) {
             reject(error)
         }
