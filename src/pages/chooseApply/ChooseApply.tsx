adc4d059e502a2afe67b12389d77ef152645074e
zyc <757715809@qq.com>
2021-12-01 17:53:35 +0800

    【开发】公告已读mock

---
+++ src/pages/chooseApply/ChooseApply.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { DetailContent, DetailTitle } from '../common';
+
+export default function ChooseApply(): React.ReactNode {
+
+    return <DetailContent>
+        <DetailTitle title="选择应用"/>
+        
+    </DetailContent>
+}
