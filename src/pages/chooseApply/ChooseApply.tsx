9ffaebba9276527451a5a2e1e8523ed8aed5b8c5
zyc <757715809@qq.com>
2021-12-02 10:34:48 +0800

    【开发】选择应用增加描述

--- src/pages/chooseApply/ChooseApply.tsx
+++ src/pages/chooseApply/ChooseApply.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { DetailContent, DetailTitle } from '../common';
 import styles from './ChooseApply.module.less';
 import { Image } from 'antd';
@@ -24,55 +24,55 @@ export default function ChooseApply(): React.ReactNode {
             authority: '',
             icon: marketing,
             title: '营销中心',
-            description: '包含合同、定档、任务等'
+            description: '包含客户、合同、订单等'
         },
         {
             authority: '',
             icon: develop,
             title: '研发中心',
-            description: '包含合同、定档、任务等'
+            description: '包含提料、放样等'
         },
         {
             authority: '',
             icon: supply,
             title: '供应中心',
-            description: '包含合同、定档、任务等'
+            description: '包含配料、采购合同、采购计划、收货单等'
         },
         {
             authority: '',
             icon: qualityInspection,
             title: '质检中心',
-            description: '包含合同、定档、任务等'
+            description: '质检过程相关功能'
         },
         {
             authority: '',
             icon: production,
             title: '生产中心',
-            description: '包含合同、定档、任务等'
+            description: '生产过程管理相关功能'
         },
         {
             authority: '',
             icon: logistic,
             title: '物流中心',
-            description: '包含合同、定档、任务等'
+            description: '配车、发货等相关功能'
         },
         {
             authority: '',
             icon: sales,
             title: '售后中心',
-            description: '包含合同、定档、任务等'
+            description: '售后服务相关功能'
         },
         {
             authority: '',
             icon: hr,
             title: '人资中心',
-            description: '包含合同、定档、任务等'
+            description: '人资管理相关功能'
         },
         {
             authority: '',
             icon: general,
             title: '通用中心',
-            description: '包含合同、定档、任务等'
+            description: '必备基础功能'
         }
     ]
     return <DetailContent>
