714dfe8a1251768e41614b39807fa01fa7b54ec0
zyc <757715809@qq.com>
2021-12-02 18:56:50 +0800

    【修改】导出undefined问题

--- src/pages/common/Page.tsx
+++ src/pages/common/Page.tsx
@@ -159,7 +159,7 @@ class Page extends AbstractMngtComponent<PageProps, PageState> {
                     url={this.props.exportPath}
                     serchObj={{
                         ...this.props.filterValue,
-                        ...this.props.requestData
+                        ...JSON.parse(JSON.stringify(this.props?.requestData))
                     }}
                     closeExportList={() => {
                         this.setState({
