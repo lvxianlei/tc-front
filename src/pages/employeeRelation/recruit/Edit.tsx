045b068d424ebed878f9588c9e54227014001782
lixy <826446332@qq.com>
2021-12-06 18:12:58 +0800

    【RZ】入职 部门班组

--- src/pages/employeeRelation/recruit/Edit.tsx
+++ src/pages/employeeRelation/recruit/Edit.tsx
@@ -168,7 +168,6 @@ export default function RecruitEdit(): React.ReactNode {
                             <Input maxLength={ 50 }  addonAfter={ <EmployeeDeptSelectionComponent onSelect={ (selectedRows: IDept[] | any) => {
                                     setSelectedDeptRows(selectedRows);
                                     form.setFieldsValue({
-                                        employeeName: selectedRows[0].employeeName,
                                         newDepartmentName: selectedRows[0].parentName+'/'+selectedRows[0].name,
                                         departmentId: selectedRows[0].parentId,
                                         teamId: selectedRows[0].id,
