e169ed5351f98e92bafc7837fa282ea2b4bc3582
lvxianlei <lvxianlei@hotmail.com>
2021-12-06 20:02:13 +0800

    员工保险档案

--- src/pages/socialSecurity/archives/Edit.tsx
+++ src/pages/socialSecurity/archives/Edit.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Button, Spin } from 'antd'
+import { Button, Spin, Form } from 'antd'
 import { useHistory, useParams } from 'react-router-dom'
 import { DetailContent, DetailTitle, BaseInfo, CommonTable } from '../../common'
 import { setting, insurance, business } from "./archives.json"
@@ -8,9 +8,11 @@ import RequestUtil from '../../../utils/RequestUtil'
 export default function Edit() {
     const history = useHistory()
     const params = useParams<{ archiveId: string, type: "new" | "edit" }>()
+    const [baseForm] = Form.useForm()
     const { loading, data } = useRequest<{ [key: string]: any }>(() => new Promise(async (resole, reject) => {
         try {
             const result: { [key: string]: any } = await RequestUtil.get(`/tower-hr/insuranceArchives/detail?id=${params.archiveId}`)
+            baseForm.setFieldsValue(result)
             resole(result)
         } catch (error) {
             reject(error)
@@ -36,9 +38,9 @@ export default function Edit() {
     ]}>
         <Spin spinning={loading}>
             <DetailTitle title="员工保险档案" />
-            <BaseInfo columns={setting} dataSource={data || {}} edit />
+            <BaseInfo columns={setting} form={baseForm} dataSource={data || {}} edit />
             <DetailTitle title="社保公积金" />
-            <BaseInfo columns={insurance} dataSource={[]} edit />
+            <BaseInfo columns={insurance} dataSource={data || {}} edit />
             <DetailTitle title="商业保险方案" />
             <CommonTable columns={business} dataSource={[]} />
         </Spin>
