e7d07928a5530f61a67c99c748bbfe18fb102cff
zyc <757715809@qq.com>
2021-12-07 17:29:31 +0800

    【sw迭代】新增其他参数配置

---
+++ src/pages/stock/otherParameters/OtherParameters.tsx
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+import { Input, DatePicker, Select, Button, Form, Modal, InputNumber, message } from 'antd';
+import { Page } from '../../common';
+import { FixedType } from 'rc-table/lib/interface';
+import { useLocation } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import RequestUtil from '../../../utils/RequestUtil';
+
+interface IOteherParameters {
+    readonly description?: string;
+    readonly id?: string;
+    readonly retaParam?: string;
+    readonly reteName?: string;
+}
+
+export default function SetOutTaskList(): React.ReactNode {
+    const [ refresh, setRefresh ] = useState<boolean>(false);
+    const history = useHistory();
+    
+    const columns = [
+        {
+            key: 'index',
+            title: '序号',
+            dataIndex: 'index',
+            width: 50,
+            render: (_: undefined, record: Record<string, any>, index: number): React.ReactNode => (<span>{ index + 1 }</span>)
+        },
+        {
+            key: 'reteName',
+            title: '名称',
+            width: 150,
+            dataIndex: 'reteName'
+        },
+        {
+            key: 'retaParam',
+            title: '参数',
+            dataIndex: 'retaParam',
+            width: 120
+        },
+        {
+            key: 'description',
+            title: '说明',
+            width: 200,
+            dataIndex: 'description'
+        },
+        {
+            key: 'operation',
+            title: '操作',
+            dataIndex: 'operation',
+            fixed: 'right' as FixedType,
+            width: 200,
+            render: (_: undefined, record: Record<string, any>): React.ReactNode => (
+                <Button type="link" onClick={ () => showModal(record.id) }>编辑</Button>
+            )
+        }
+    ]
+
+    const showModal = async (id: string) => {
+        const data = await RequestUtil.get<IOteherParameters>(`/tower-supply/materialRate/${ id }`);
+        setDetailData(data);
+        form.setFieldsValue({...data});
+        setVisible(true);
+    }
+
+    const close = () => {
+        setVisible(false); 
+        setDetailData({}); 
+        form.resetFields();
+    }
+
+    const save = () => {
+        if(form) {
+            form.validateFields().then(res => {
+                const values = form.getFieldsValue(true);
+                RequestUtil.post('/tower-supply/materialRate', {
+                    ...detailData,
+                    ...values
+                }).then(res => {
+                    close();
+                    setRefresh(!refresh);
+                    message.success('保存成功');
+                })
+            })
+        }
+    }
+    
+    const [ detailData, setDetailData ] = useState<IOteherParameters>({});  
+    const [ form ] = Form.useForm();
+    const [ visible, setVisible ] = useState(false);
+
+    return <>
+        <Page
+            path="/tower-supply/materialRate"
+            columns={ columns }
+            headTabs={ [] }
+            refresh={ refresh }
+            extraOperation={<Button type="primary" onClick={() => history.goBack()} ghost>返回上一级</Button>}
+            searchFormItems={ [] }
+        />
+        <Modal visible={ visible } title="编辑" onCancel={ close } onOk={ save }>
+            <Form form={ form }>
+                <Form.Item label="税率参数" name="retaParam" rules={[{
+                        required: true,
+                        message: '请输入税率参数'
+                    }]}>
+                    <InputNumber min={ 0 } step="0.01" precision={ 2 } max={ 99.99 } style={{ width: '100%' }}/>
+                </Form.Item>
+            </Form>
+        </Modal>
+    </>
+}
