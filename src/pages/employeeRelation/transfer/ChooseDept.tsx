2e09c3af1083fcc562703c8053a8c07da65212f7
lvxianlei <lvxianlei@hotmail.com>
2021-12-07 10:11:09 +0800

    合并

--- src/pages/employeeRelation/transfer/ChooseDept.tsx
+++ src/pages/employeeRelation/transfer/ChooseDept.tsx
@@ -87,6 +87,8 @@ export const PopTableContent: React.FC<{ data: PopTableData, value?: { id: strin
         setColumns(data.columns)
     }, [JSON.stringify(data.columns)])
 
+    
+
     return <>
         {searchs.length > 0 && <Form form={form} onFinish={async () => {
             await run()
@@ -157,7 +159,6 @@ const PopTable: React.FC<PopTableProps> = ({ data, ...props }) => {
 
     const formatValue = () => {
         let initValue = typeof props.value === "string" ? props.value : value?.value
-        console.log(value,"--------")
         return initValue
     }
 
