395b115bdf63ce1ba61095ea2e08caefa524f2c5
lixy <826446332@qq.com>
2021-12-14 14:15:10 +0800

    【修复】bug3100

--- src/pages/planBaseSetting/planProd/PlanProd.tsx
+++ src/pages/planBaseSetting/planProd/PlanProd.tsx
@@ -37,7 +37,7 @@ class PlanGantt extends React.Component<IWithSectionModalRouteProps, WithSection
         gantt.clearAll();
         gantt.config.column_width = 20;
         gantt.config.columns = [
-          {label:'计划号', name: "planNumber", tree: true, resize: true , width:200, template: function (task:any) {
+          {label:'计划号', name: "planNumber", tree: true, resize: true , width:170, template: function (task:any) {
             if(!task.parent){
               // href='/planProd/planMgmt/detail/${task.id}/${task.planId}'
               return (
@@ -48,7 +48,7 @@ class PlanGantt extends React.Component<IWithSectionModalRouteProps, WithSection
             }
               
           }},
-          {label:'塔型',name: "name", align: "center", resize: true, width: 100},
+          {label:'塔型',name: "name", align: "center", resize: true, width: 130},
           {label:'基数',name: "productNum", align: "center"},
           {label:'重量',name: "weight", align: "center"},
           {label:'交货日期',name: "deliveryTime", align: "center"},
