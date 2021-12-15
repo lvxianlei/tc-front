e7e08f4876c0fdd98cd8853b12462ae8bf482982
lixy <826446332@qq.com>
2021-12-15 11:16:20 +0800

    【修复】bug3146 3143

--- src/pages/planBaseSetting/prodUnit/add.tsx
+++ src/pages/planBaseSetting/prodUnit/add.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState, } from 'react'
-import { Button, DatePicker, Input, message, Modal, Select, } from 'antd';
+import { Button, Col, DatePicker, Form, Input, InputNumber, message, Modal, Row, Select, } from 'antd';
 import RequestUtil from '../../../utils/RequestUtil';
 import * as echarts from 'echarts';
 import './prodUnit.less';
@@ -10,6 +10,7 @@ import dayjs from 'dayjs';
 const ProdUnitAdd = (props: any) => {
     let [prodLinkList, setProdLinkList] = useState<any[]>([])
     let [itemInfo, setItemInfo] = useState<any>({})
+    const [form] = Form.useForm();
     // let [times, setTimes] = useState<string[]>(['', ''])
     const [value, setValue] = useState<any>([moment(dayjs().format('YYYY-MM-DD')), moment(dayjs().add(6, 'day').format('YYYY-MM-DD'))]);
     const [dates, setDates] = useState<any>([]);
@@ -46,7 +47,10 @@ const ProdUnitAdd = (props: any) => {
         data['productionLinkDTOList'] = data['productionLinkVOList'].map((item: { productionLinkId: string }) => {
             return item.productionLinkId
         })
-        setItemInfo(data)
+        // setItemInfo(data)
+        form.setFieldsValue({
+            ...data
+        })
     }
     /**
      * @description 获取生产环节
@@ -73,21 +77,37 @@ const ProdUnitAdd = (props: any) => {
      * @description 弹窗提交
      */
     const submit = async () => {
-        if(itemInfo.productivity&&itemInfo.name&&itemInfo['productionLinkDTOList']&&itemInfo['productionLinkDTOList'].length>0){
-            itemInfo['productionLinkDTOList'] = itemInfo['productionLinkDTOList'].map((item: string) => {
+        await form.validateFields();
+        const value = form.getFieldsValue(true);
+        const submitValue = {
+            ...value,
+            productionLinkDTOList: value.productionLinkDTOList.map((item: string) => {
                 return {
                     productionLinkId: item,
                     productionUnitId: props.id
                 }
             })
-            await RequestUtil.post('/tower-aps/productionUnit', {
-                ...itemInfo,
-            })
-            message.success('操作成功')
-            props.cancelModal(true)
-        }else{
-            message.error('必填项未填写，不可提交！')
         }
+        await RequestUtil.post('/tower-aps/productionUnit', {
+            ...submitValue,
+        })
+        message.success('操作成功')
+        props.cancelModal(true)
+        // if(itemInfo.productivity&&itemInfo.name&&itemInfo['productionLinkDTOList']&&itemInfo['productionLinkDTOList'].length>0){
+        //     itemInfo['productionLinkDTOList'] = itemInfo['productionLinkDTOList'].map((item: string) => {
+        //         return {
+        //             productionLinkId: item,
+        //             productionUnitId: props.id
+        //         }
+        //     })
+        //     await RequestUtil.post('/tower-aps/productionUnit', {
+        //         ...itemInfo,
+        //     })
+        //     message.success('操作成功')
+        //     props.cancelModal(true)
+        // }else{
+        //     message.error('必填项未填写，不可提交！')
+        // }
         
     }
     /**
@@ -220,13 +240,13 @@ const ProdUnitAdd = (props: any) => {
                 cancelText='取消'
                 okText='确定'
             >
-                <div>
+                {/* <div>
                     <div className='edit-item'>
                         <span className='tip' style={{ width: 110, }}>生产单元名称*：</span>
                         <Input
                             className='input'
                             maxLength={24}
-                            value={itemInfo['name']}
+                            defaultValue={itemInfo['name']}
                             onChange={(ev) => {
                                 changeItemInfo(ev.target.value.trim(), 'name')
                             }}
@@ -238,9 +258,9 @@ const ProdUnitAdd = (props: any) => {
                         <Input
                             className='input'
                             maxLength={12}
-                            value={itemInfo['productivity']}
+                            defaultValue={itemInfo['productivity']}
                             onChange={(ev) => {
-                                changeItemInfo(ev.target.value.replace(/[^0-9]/ig, ""), 'productivity')
+                                // changeItemInfo(ev.target.value.replace(/[^0-9]/ig, ""), 'productivity')
                             }}
                             placeholder='请输入'
                         />
@@ -250,7 +270,7 @@ const ProdUnitAdd = (props: any) => {
                         <Select
                             className='input'
                             placeholder='请选择'
-                            value={itemInfo['productionLinkDTOList'] || []}
+                            defaultValue={itemInfo['productionLinkDTOList'] || []}
                             mode='multiple'
                             maxTagCount={10}
                             searchValue=''
@@ -271,7 +291,52 @@ const ProdUnitAdd = (props: any) => {
                             }
                         </Select>
                     </div>
-                </div>
+                </div> */}
+                <Form form={ form }>
+                    <Row>
+                        <Col  span={24}>
+                            <Form.Item label="生产单元名称" rules={[{required:true,message:'请填写生产单元名称'}]} name='name'>
+                                <Input/>
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col  span={24}>
+                            <Form.Item label="产力值" rules={[{required:true,message:'请填写产力值'}]} name='productivity'>
+                                <InputNumber maxLength={12} min={0} style={{width:'100%'}} />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col  span={24}>
+                            <Form.Item label="关联生产环节" rules={[{required:true,message:'请选择关联生产环节'}]} name='productionLinkDTOList'>
+                                <Select
+                                    className='input'
+                                    placeholder='请选择'
+                                    mode='multiple'
+                                    maxTagCount={10}
+                                    searchValue=''
+                                    // onChange={(value) => {
+                                    //     changeItemInfo(value, 'productionLinkDTOList')
+                                    // }}
+                                >
+                                    {
+                                        prodLinkList.map((item: any, index: number) => {
+                                            return (
+                                                <Select.Option
+                                                    key={index}
+                                                    value={item.id}
+                                                    disabled={item.isUse}
+                                                >{item.name}</Select.Option>
+                                            )
+                                        })
+                                    }
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                
+                </Form>
                 <div className='see' hidden={!props.id}>
                     <Button
                         onClick={() => {
