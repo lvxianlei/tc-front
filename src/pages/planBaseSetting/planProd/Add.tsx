5729d13a36815c3a875e400704466b3bb7b8792a
lixy <826446332@qq.com>
2021-12-15 11:57:54 +0800

    【修复】bug3163

--- src/pages/planBaseSetting/planProd/Add.tsx
+++ src/pages/planBaseSetting/planProd/Add.tsx
@@ -272,7 +272,7 @@ export default function RecruitEdit(): React.ReactNode {
                 <Row>
                     <Col span={12}>
                         <Form.Item label="完成天数" rules={[{required:true,message:'请填写完成天数'}]} name='minCompletionDays'>
-                            <InputNumber maxLength={10} min={0} style={{width:'100%'}} onChange={
+                            <InputNumber maxLength={10} min={1} style={{width:'100%'}} precision={0} onChange={
                                 e=>{
                                     const value = form.getFieldsValue().startTime
                                     if(value){
