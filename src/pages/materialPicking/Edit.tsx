2f495545e0566604666337358c1cd1fb5a99704d
lvxianlei <lvxianlei@hotmail.com>
2021-12-21 18:12:16 +0800

    MC 原材料领料单

---
+++ src/pages/materialPicking/Edit.tsx
@@ -0,0 +1,79 @@
+import React, { useImperativeHandle, forwardRef, useState } from "react"
+import { Button, Form, Spin } from 'antd'
+import { useHistory, useParams } from 'react-router-dom'
+import { DetailContent, DetailTitle, BaseInfo, CommonTable } from '../common'
+import { setting, materialInfo } from "./picking.json"
+import RequestUtil from '../../utils/RequestUtil'
+import useRequest from '@ahooksjs/use-request'
+export interface EditProps {
+    type: "new" | "edit",
+    ref?: React.RefObject<{ onSubmit: () => Promise<any> }>
+    id: string
+}
+
+export default forwardRef(function Edit({ type, id }: EditProps, ref) {
+    const params = useParams<{ id: string }>()
+    const history = useHistory()
+    const [baseForm] = Form.useForm()
+    const { loading, data } = useRequest<{ [key: string]: any }>(() => new Promise(async (resole, reject) => {
+        try {
+            const result: { [key: string]: any } = await RequestUtil.get(`/tower-market/invoicing/getInvoicingInfo/${params.id}`)
+            resole(result)
+        } catch (error) {
+            reject(error)
+        }
+    }), { manual: type === "new" })
+
+    const { run: saveRun } = useRequest<{ [key: string]: any }>((id) => new Promise(async (resole, reject) => {
+        try {
+            const result: { [key: string]: any } = await RequestUtil.get(`/tower-market/taskNotice/getLogicWeightByContractId?contractId=
+            ${id}`)
+            resole(result)
+        } catch (error) {
+            reject(error)
+        }
+    }), { manual: true })
+
+
+    const onSubmit = (saveType?: "save" | "saveAndApply") => new Promise(async (resolve, reject) => {
+        try {
+            const baseData = await baseForm.validateFields()
+            const postData = type === "new" ? {
+                ...baseData,
+
+            } : {
+                ...baseData,
+
+            }
+            await saveRun(postData, saveType)
+            resolve(true)
+        } catch (error) {
+            reject(false)
+        }
+    })
+
+    const handleBaseInfoChange = async (fields: any) => {
+        if (fields.contractCode) {
+            const contractValue = fields.contractCode.records[0]
+            baseForm.setFieldsValue({
+
+            })
+        }
+    }
+
+    useImperativeHandle(ref, () => ({ onSubmit }), [ref, onSubmit])
+
+    return <Spin spinning={loading}>
+        <DetailTitle title="基础信息" />
+        <BaseInfo
+            onChange={handleBaseInfoChange}
+            form={baseForm}
+            columns={setting}
+            col={3}
+            dataSource={{}} edit />
+        <DetailTitle title="原材料信息" operation={[
+            <Button type="primary" ghost>选择原材料</Button>
+        ]} />
+        <CommonTable columns={materialInfo} dataSource={[]} />
+    </Spin>
+})
---
+++ src/pages/materialPicking/index.tsx
@@ -0,0 +1,166 @@
+import React, { useRef, useState } from "react"
+import { Button, Input, DatePicker, Select, Modal, message } from 'antd'
+import { Link, useHistory } from 'react-router-dom'
+import { Page } from '../common'
+import { picking } from "./picking.json"
+import useRequest from '@ahooksjs/use-request'
+import RequestUtil from '../../utils/RequestUtil'
+import Edit from "./Edit"
+import OverView from "./Overview"
+
+interface EditRefProps {
+    onSubmit: (type?: "saveAndApply" | "save") => void
+    resetFields: () => void
+}
+
+export default function Invoicing() {
+    const editRef = useRef<EditRefProps>()
+    const [type, setType] = useState<"new" | "edit">("new")
+    const [visible, setVisible] = useState<boolean>(false)
+    const [detailVisible, setDetailVisible] = useState<boolean>(false)
+    const [detailId, setDetailId] = useState<string>("")
+    const history = useHistory()
+    const [filterValue, setFilterValue] = useState({});
+    const { run: deleteRun } = useRequest<{ [key: string]: any }>((id: string) => new Promise(async (resole, reject) => {
+        try {
+            const result: { [key: string]: any } = await RequestUtil.delete(`/tower-market/invoicing?id=${id}`)
+            resole(result)
+        } catch (error) {
+            reject(error)
+        }
+    }), { manual: true })
+
+    const onFilterSubmit = (value: any) => {
+        if (value.startLaunchTime) {
+            const formatDate = value.startLaunchTime.map((item: any) => item.format("YYYY-MM-DD"))
+            value.startLaunchTime = formatDate[0]
+            value.endLaunchTime = formatDate[1]
+        }
+        setFilterValue(value)
+        return value
+    }
+
+    const handleDelete = (id: string) => {
+        Modal.confirm({
+            title: "删除",
+            content: "确定删除此领料单吗？",
+            onOk: () => new Promise(async (resove, reject) => {
+                try {
+                    resove(await deleteRun(id))
+                    message.success("删除成功...")
+                    history.go(0)
+                } catch (error) {
+                    reject(error)
+                }
+            })
+        })
+    }
+    const handleModalOk = (type?: "saveAndApply" | "save") => new Promise(async (resove, reject) => {
+        try {
+            await editRef.current?.onSubmit(type)
+            message.success("票据创建成功...")
+            setVisible(false)
+            history.go(0)
+            resove(true)
+        } catch (error) {
+            reject(false)
+        }
+    })
+    return <>
+        <Modal visible={visible} destroyOnClose
+            width={1011} title={type === "new" ? "新增领料单" : "编辑领料单"}
+            footer={[
+                <Button key="saveOr" type="primary" onClick={() => handleModalOk("saveAndApply")} >保存并提交</Button>,
+                <Button key="save" type="primary" onClick={() => handleModalOk()}>保存</Button>,
+                <Button
+                    key="close"
+                    type="primary"
+                    ghost
+                    onClick={async () => {
+                        setDetailId("")
+                        setType("new")
+                        setVisible(false)
+                    }}>关闭</Button>
+            ]}
+            onCancel={() => {
+                setDetailId("")
+                setType("new")
+                setVisible(false)
+            }}>
+            <Edit type={type} ref={editRef} id={detailId} />
+        </Modal>
+        <Modal
+            destroyOnClose
+            visible={detailVisible} width={1011}
+            footer={<Button type="primary" onClick={() => {
+                setDetailId("")
+                setDetailVisible(false)
+            }}>确认</Button>}
+            title="详情"
+            onCancel={() => {
+                setDetailId("")
+                setDetailVisible(false)
+            }}>
+            <OverView id={detailId} />
+        </Modal>
+        <Page
+            path=""
+            filterValue={filterValue}
+            columns={[
+                ...picking,
+                {
+                    title: "操作",
+                    dataIndex: "opration",
+                    fixed: "right",
+                    width: 100,
+                    render: (_: any, record: any) => {
+                        return <>
+                            <Button type="link" size="small" onClick={() => {
+
+                            }}>详情</Button>
+                            <Button type="link" size="small" disabled={![0, 3].includes(record.state)} onClick={() => {
+
+                            }}>编辑</Button>
+                            <Button type="link" size="small" disabled={record.state !== 0} onClick={() => handleDelete(record.id)}>删除</Button>
+                            <Button type="link" size="small" disabled={record.state !== 0} onClick={() => handleDelete(record.id)}>撤回</Button>
+                        </>
+                    }
+                }]}
+            extraOperation={<Button type="primary" onClick={() => {
+                setDetailId("")
+                setType("new")
+                setVisible(true)
+            }}>新增领料单</Button>}
+            onFilterSubmit={onFilterSubmit}
+            searchFormItems={[
+                {
+                    name: 'fuzzyQuery',
+                    children: <Input placeholder="工程名称/销售计划号/塔型/加工计划编号/领料人" style={{ width: 300 }} />
+                },
+                {
+                    name: 'isOpen',
+                    label: '领料生产单元',
+                    children: <Select style={{ width: 200 }}>
+                        <Select.Option value="2">发票未开全</Select.Option>
+                        <Select.Option value="3">发票已开全</Select.Option>
+                    </Select>
+                },
+                {
+                    name: 'contractType',
+                    label: '状态',
+                    children: <Select style={{ width: 200 }}>
+                        <Select.Option value="1">已创建</Select.Option>
+                        <Select.Option value="2">已撤回</Select.Option>
+                        <Select.Option value="3">已放料</Select.Option>
+                        <Select.Option value="4">未放料</Select.Option>
+                    </Select>
+                },
+                {
+                    name: 'startLaunchTime',
+                    label: '申请日期',
+                    children: <DatePicker.RangePicker format="YYYY-MM-DD" />
+                }
+            ]}
+        />
+    </>
+}
