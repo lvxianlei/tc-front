b30f7f57913c8b4a98b8dc65c6877b86f2bb33d9
cjl <249198222@qq.com>
2021-12-22 19:45:14 +0800

    计划排产 状态更改

--- src/pages/planBaseSetting/planProd/Add.tsx
+++ src/pages/planBaseSetting/planProd/Add.tsx
@@ -50,10 +50,10 @@ settypenum(data.status)
             productionLinkId: data.linkId
         })
 
-        // const listValue: any = params.productCategoryId && value.records.length > 0 ? value.records.filter((res: any) => { return res.id === data.unitId }) : [{}]
+        const listValue: any = params.productCategoryId && value.records.length > 0 ? value.records.filter((res: any) => { return res.id === data.unitId }) : [{}]
 
-        // params.productCategoryId && setProductivity(listValue[0].productivity?listValue[0].productivity:'')
-        // params.productCategoryId&& seeLoad(listValue[0].productivity, data.unitId)
+        params.productCategoryId && setProductivity(listValue[0].productivity?listValue[0].productivity:'')
+        params.productCategoryId&& seeLoad(listValue[0].productivity, data.unitId)
         params.productCategoryId && setProdUnitList(value.records)
         form.setFieldsValue(params.productCategoryId ? {
             ...data,
