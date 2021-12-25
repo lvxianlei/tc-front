03a77f1ce4532366f26593f142f103f0d1ea9f4a
lvxianlei <lvxianlei@hotmail.com>
2021-12-25 13:36:48 +0800

    合同回款统计区分环境

--- src/pages/receivables/constract/index.tsx
+++ src/pages/receivables/constract/index.tsx
@@ -1,10 +1,10 @@
 import React from "react"
 const iframeSrc: any = {
-    "development": "http://39.106.2.24:8088/link/1",
+    "integration": "http://39.106.2.24:8088/link/1",
     "test": "http://39.106.2.24:8088/link/1",
     "uat": "http://39.106.2.24:8088/link/2",
 }
 export default function Constract(): JSX.Element {
-    console.log(process.env.NODE_ENV)
-    return <iframe style={{ width: "100%", height: "100%", border: "none" }} src={iframeSrc[process.env.NODE_ENV]} />
+    console.log(process.env.REACT_APP_ENV)
+    return <iframe style={{ width: "100%", height: "100%", border: "none" }} src={iframeSrc[process.env.REACT_APP_ENV || "integration"]} />
 }
