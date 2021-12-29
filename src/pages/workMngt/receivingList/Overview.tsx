2426f5081693d6283cfbcb6a9a521165229cb86e
齐广广 <13641315679@163.com>
2021-12-29 17:42:52 +0800

    解决bug3954

--- src/pages/workMngt/receivingList/Overview.tsx
+++ src/pages/workMngt/receivingList/Overview.tsx
@@ -4,6 +4,7 @@ import { useHistory, useParams } from 'react-router-dom'
 import { DetailContent, CommonTable, Attachment, AttachmentRef } from '../../common'
 import { CargoDetails } from "./receivingListData.json"
 import useRequest from '@ahooksjs/use-request'
+import { Page } from '../../common';
 import RequestUtil from '../../../utils/RequestUtil'
 
 interface ReceiveStrokAttachProps {
@@ -49,29 +50,24 @@ export default function Edit() {
     const [attchType, setAttachType] = useState<1 | 2>(1)
     const [detailId, setDetailId] = useState<string>("")
     const [saveLoding, setSaveLoading] = useState<boolean>(false)
-    const [form] = Form.useForm()
-    const { loading, data } = useRequest<{ [key: string]: any }>(() => new Promise(async (resole, reject) => {
-        try {
-            const v: any = {
-                receiveStockId: params.id,
-            }
-            if (filterValue) {
-                const formatDate = filterValue.startStatusUpdateTime.map((item: any) => item.format("YYYY-MM-DD"))
-                v.startStatusUpdateTime = `${formatDate[0]} 00:00:00`
-                v.endStatusUpdateTime = `${formatDate[1]} 23:59:59`
-            }
-            const result: { [key: string]: any } = await RequestUtil.get(`/tower-storage/receiveStock/detail`, v)
-            resole(result)
-        } catch (error) {
-            reject(error)
-        }
-    }), { refreshDeps: [JSON.stringify(filterValue)] })
     const handleAttachOk = async () => {
         setSaveLoading(true)
         await receiveRef.current.onSubmit()
         setSaveLoading(false)
         message.success("保存成功...")
         setVisible(false)
+    }
+    // 查询按钮
+    const onFilterSubmit = (value: any) => {
+        if (value.startStatusUpdateTime) {
+            const formatDate = value.startRefundTime.map((item: any) => item.format("YYYY-MM-DD"))
+            value.startStatusUpdateTime = `${formatDate[0]} 00:00:00`
+            value.endStatusUpdateTime = `${formatDate[1]} 23:59:59`
+            delete value.startRefundTime
+        }
+        value["receiveStockId"] = params.id;
+        setFilterValue({...value, receiveStockId: params.id})
+        return value
     }
     return <DetailContent>
         <Modal
@@ -88,45 +84,72 @@ export default function Edit() {
             }}>
             <ReceiveStrokAttach type={attchType} id={detailId} ref={receiveRef} />
         </Modal>
-        <Form form={form} onFinish={(values) => setFilterValue(values)}>
-            <Row>
-                <Col><Form.Item label="最新状态变更时间" name="startStatusUpdateTime"><DatePicker.RangePicker format="YYYY-MM-DD" /></Form.Item></Col>
-                <Col><Form.Item label="采购状态" name="receiveStatus">
-                    <Select defaultValue="全部" style={{ width: 150 }}>
+        <Page
+            path="/tower-storage/receiveStock/detail"
+            exportPath={"/tower-storage/receiveStock/detail"}
+            exportObject={{ receiveStockId: params.id }}
+            onFilterSubmit={onFilterSubmit}
+            filterValue={filterValue}
+            searchFormItems={[
+                {
+                    name: 'startStatusUpdateTime',
+                    label: "最新状态变更时间",
+                    children: <DatePicker.RangePicker format="YYYY-MM-DD" />
+                },
+                {
+                    name: 'receiveStatus',
+                    label: '采购状态',
+                    children: <Form.Item name="receiveStatus">
+                        <Select defaultValue="全部" style={{ width: 150 }}>
                         <Select.Option value="">全部</Select.Option>
                         <Select.Option value={0}>待收货</Select.Option>
                         <Select.Option value={1}>已收货</Select.Option>
                         <Select.Option value={2}>已拒绝</Select.Option>
                     </Select>
-                </Form.Item></Col>
-                <Col><Form.Item>
-                    <Button type="primary" htmlType="submit" style={{ marginLeft: 12 }}>搜索</Button>
-                    <Button type="default" htmlType="reset" style={{ marginLeft: 12 }}>重置</Button>
-                </Form.Item></Col>
-            </Row>
-        </Form>
-        <Row>
-            <Button type="primary" ghost>导出</Button>
-            <Button type="primary" ghost onClick={() => message.warning("功能开发中...")} style={{ marginRight: 16, marginLeft: 16 }}>申请质检</Button>
-            <Button type="primary" ghost onClick={() => history.goBack()}>返回</Button>
-        </Row>
-        <Row style={{ lineHeight: "32px", fontWeight: 600 }} gutter={10}>
-            <Col>已收货：重量(支)合计：{data?.receiveStockMessage.receiveWeight === -1 ? 0 : data?.receiveStockMessage.receiveWeight}</Col>
-            <Col>价税合计(元)合计：{data?.receiveStockMessage.receivePrice === -1 ? 0 : data?.receiveStockMessage.receivePrice}</Col>
-            <Col>待收货：重量(支)合计：{data?.receiveStockMessage.waitWeight === -1 ? 0 : data?.receiveStockMessage.waitWeight}</Col>
-            <Col>价税合计(元)合计：{data?.receiveStockMessage.waitPrice === -1 ? 0 : data?.receiveStockMessage.waitPrice}</Col>
-        </Row>
-        <CommonTable loading={loading} haveIndex columns={[...CargoDetails, {
+                    </Form.Item>
+                }
+            ]}
+            sourceKey="receiveStockDetailPage.records"
+            extraOperation={(data: any) => {
+                return <>
+                <Button type="primary" ghost onClick={() => message.warning("功能开发中...")} style={{ marginRight: 16, marginLeft: 16 }}>申请质检</Button>
+                <Button type="primary" ghost onClick={() => history.goBack()}>返回</Button>
+                <span style={{marginLeft:"20px"}}>
+                    已收货：重量(支)合计：{data?.receiveStockMessage.receiveWeight === -1 ? 0 : data?.receiveStockMessage.receiveWeight}
+                    价税合计(元)合计：{data?.receiveStockMessage.receivePrice === -1 ? 0 : data?.receiveStockMessage.receivePrice}
+                    待收货：重量(支)合计：{data?.receiveStockMessage.waitWeight === -1 ? 0 : data?.receiveStockMessage.waitWeight}
+                    价税合计(元)合计：{data?.receiveStockMessage.waitPrice === -1 ? 0 : data?.receiveStockMessage.waitPrice}
+                </span>
+            </>
+            }}
+            columns={[
+                ...CargoDetails,
+                {
+                    title: "操作",
+                    dataIndex: "opration",
+                    fixed: "right",
+                    width: 100,
+                    render: (_: any, records: any) => <>
+                    <a style={{marginRight: 12}} onClick={() => {
+                        setAttachType(1)
+                        setDetailId(records.id)
+                        setVisible(true)
+                    }}>质保单</a>
+                    <Button type="link" onClick={() => message.warning("功能开发中...")}>质检单</Button>
+                </>
+            }]}
+        />
+        {/* <CommonTable loading={loading} haveIndex columns={[...CargoDetails, {
             title: "操作",
             dataIndex: "opration",
             render: (_: any, records: any) => <>
-                <a onClick={() => {
+                <a style={{marginRight: 12}} onClick={() => {
                     setAttachType(1)
                     setDetailId(records.id)
                     setVisible(true)
                 }}>质保单</a>
                 <Button type="link" onClick={() => message.warning("功能开发中...")}>质检单</Button>
             </>
-        }]} dataSource={data?.receiveStockDetailPage?.records || []} />
+        }]} dataSource={data?.receiveStockDetailPage?.records || []} /> */}
     </DetailContent>
 }
