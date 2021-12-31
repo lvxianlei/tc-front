69c21ee1fe25ce134ea29af294c0857874bc673f
lvxianlei <lvxianlei@hotmail.com>
2021-12-31 16:03:42 +0800

    缺料列表-操作记录

--- src/pages/workMngt/shortageList/Overview.tsx
+++ src/pages/workMngt/shortageList/Overview.tsx
@@ -1,23 +1,10 @@
 import React from "react"
-import { Button, message, Spin } from 'antd'
-import { useHistory, useParams } from 'react-router-dom'
-import { DetailContent, DetailTitle, BaseInfo, CommonTable } from '../../common'
-import { operationInformation } from "./shortageListData.json"
-import useRequest from '@ahooksjs/use-request'
-import RequestUtil from '../../../utils/RequestUtil'
+
+import { OperationRecord } from '../../common'
 interface OverviewProps {
     id: string
 }
 export default function Overview({ id }: OverviewProps) {
-    const history = useHistory()
-    const { loading, data } = useRequest<any[]>(() => new Promise(async (resole, reject) => {
-        try {
-            const result: any[] = await RequestUtil.get(`/tower-market/invoicing/getInvoicingInfo/${id}`)
-            resole(result)
-        } catch (error) {
-            reject(error)
-        }
-    }))
 
-    return <CommonTable haveIndex loading={loading} columns={operationInformation} dataSource={data || []} />
+    return <OperationRecord title={false} serviceId={id} serviceName="tower-supply" />
 }
