a66af805d2ca08abb5dedf793cbfe2ac77b709f4
lixy <826446332@qq.com>
2022-01-10 17:13:10 +0800

    【修复】调整

--- src/pages/setOutTower/AssemblyWeldList.tsx
+++ src/pages/setOutTower/AssemblyWeldList.tsx
@@ -24,15 +24,54 @@ export default function AssemblyWeldInfo(): React.ReactNode {
         pageSize: 10
     };
     const columns = [
-        { title: '序号', dataIndex: 'index', key: 'index', render: (_a: any, _b: any, index: number): React.ReactNode => (<span>{index + 1}</span>) },
-        { title: '段号', dataIndex: 'segmentName', key: 'segmentName', },
-        { title: '组件号', dataIndex: 'componentId', key: 'componentId' },
-        { title: '主件号', dataIndex: 'mainPartId', key: 'mainPartId' },
-        { title: '单段组数', dataIndex: 'singleNum', key: 'singleNum' },
-        { title: '单组重量（kg）', dataIndex: 'singleGroupWeight', key: 'singleGroupWeight' },
-        { title: '电焊米数（mm）', dataIndex: 'electricWeldingMeters', key: 'electricWeldingMeters' },
-        { title: '件号信息', dataIndex: 'numInformation', key: 'numInformation' },
-        { title: '备注', dataIndex: 'description', key: 'description' },
+        { 
+            title: '序号', 
+            dataIndex: 'index', 
+            key: 'index', 
+            render: (_a: any, _b: any, index: number): React.ReactNode => (
+                <span>{index + 1}</span>
+            ) 
+        },
+        { 
+            title: '段号', 
+            dataIndex: 'segmentName', 
+            key: 'segmentName', 
+        },
+        { 
+            title: '组件号', 
+            dataIndex: 'componentId', 
+            key: 'componentId' 
+        },
+        { 
+            title: '主件号', 
+            dataIndex: 'mainPartId', 
+            key: 'mainPartId' 
+        },
+        { 
+            title: '单段组数', 
+            dataIndex: 'singleNum', 
+            key: 'singleNum' 
+        },
+        { 
+            title: '单组重量（kg）', 
+            dataIndex: 'singleGroupWeight', 
+            key: 'singleGroupWeight' 
+        },
+        { 
+            title: '电焊米数（mm）', 
+            dataIndex: 'electricWeldingMeters', 
+            key: 'electricWeldingMeters' 
+        },
+        { 
+            title: '件号信息', 
+            dataIndex: 'numInformation', 
+            key: 'numInformation' 
+        },
+        { 
+            title: '备注', 
+            dataIndex: 'description', 
+            key: 'description' 
+        },
     ]
     const getTableDataSource = (pagination: TablePaginationConfig) => new Promise(async (resole, reject) => {
         const data = await RequestUtil.get<IResponseData>(`/tower-science/welding/getWeldingByProductSegmentId`, { ...pagination, productSegmentId:params.id });
