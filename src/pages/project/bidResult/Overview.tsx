14b13763468b0fc7aa9027b709377e2a285a3c02
齐广广 <13641315679@163.com>
2022-01-19 10:41:16 +0800

    导出

--- src/pages/project/bidResult/Overview.tsx
+++ src/pages/project/bidResult/Overview.tsx
@@ -14,7 +14,8 @@ export default function Overview({ id }: OverviewProps) {
     const history = useHistory()
     const match = useRouteMatch()
     const location = useLocation<{ state: {} }>();
-    const [isExport, setIsExportStoreList] = useState(false)
+    const [isExport, setIsExportStoreList] = useState(false);
+    const [round, setRound] = useState<number>(0); // 轮数
     const { loading, data } = useRequest<{ result: any, statistics: any }>(() => new Promise(async (resole, reject) => {
         try {
             const result: { [key: string]: any } = await RequestUtil.get(`/tower-market/bidBase/${id}`)
@@ -27,6 +28,7 @@ export default function Overview({ id }: OverviewProps) {
 
     const handleChange = (activeKey: any) => {
         console.log(activeKey, "activeKey", data?.result?.bidOpenRecordListVos)
+        setRound(data?.result?.bidOpenRecordListVos[activeKey].round)
     }
 
     return <DetailContent operation={[
@@ -38,7 +40,14 @@ export default function Overview({ id }: OverviewProps) {
             <DetailTitle title="基本信息" style={{ padding: "0 0 8px 0", }} />
             <BaseInfo columns={overview} dataSource={data?.result || {}} col={2} />
             <DetailTitle title="开标信息"  operation={[
-                <Button type="primary" ghost>导出</Button>
+                <Button type="primary" ghost onClick={() => {
+                    RequestUtil.get(`/tower-market/bidBase/exportBidOpen`, {
+                        bidBaseId: id,
+                        round: round === 0 ? data?.result?.bidOpenRecordListVos[0].round : round
+                    }).then(res => {
+                        console.log(res, "ress")
+                    });
+                }}>导出</Button>
             ]}/>
             <Tabs onChange={handleChange}>
                 {data?.result?.bidOpenRecordListVos?.length > 0 && data?.result?.bidOpenRecordListVos.map((item: any, index: number) => <Tabs.TabPane key={index}
