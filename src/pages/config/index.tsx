63fd31562873751c40cfc062fd05acc3c0aa4699
齐广广 <13641315679@163.com>
2022-01-26 18:23:57 +0800

    解决bug5094

--- src/pages/config/index.tsx
+++ src/pages/config/index.tsx
@@ -22,12 +22,12 @@ export default function Stock() {
             }
         ]}
             dataSource={[
-                { name: "仓库管理", path: "/stock/warehouse", id: "warehouse" },
-                { name: "原材料类型管理", path: "/sys/materialType", id: "materialType" },
-                { name: "原材料管理", path: "/sys/material", id: "material" },
-                { name: "角钢配置策略", path: "/stock/angleSteel", id: "angleSteel" },
-                { name: "安全库存配置", path: "/stock/securitySetting", id: "securitySetting" },
-                { name: "其他参数配置", path: "/stock/otherParameters", id: "otherParameters" }
+                { name: "仓库管理", path: "/config/configList/warehouse", id: "warehouse" },
+                { name: "原材料类型管理", path: "/config/configList/materialType", id: "materialType" },
+                { name: "原材料管理", path: "/config/configList/material", id: "material" },
+                { name: "角钢配置策略", path: "/config/configList/angleSteel", id: "angleSteel" },
+                { name: "安全库存配置", path: "/config/configList/securitySetting", id: "securitySetting" },
+                { name: "其他参数配置", path: "/config/configList/otherParameters", id: "otherParameters" }
             ]} />
     </DetailContent>
 }
