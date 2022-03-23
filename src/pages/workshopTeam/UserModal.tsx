98f00347d14b0146ef87acf87c064ad3dddb449a
lixy <826446332@qq.com>
2022-03-23 11:27:10 +0800

    【修复】bug6511

--- src/pages/workshopTeam/UserModal.tsx
+++ src/pages/workshopTeam/UserModal.tsx
@@ -1,162 +1,3 @@
-// /**
-//  * @author zyc
-//  * @copyright © 2021
-//  */
-//  import { FormItemProps, Input, Select } from 'antd';
-//  import { ColumnType, TablePaginationConfig } from 'antd/lib/table';
-//  import { GetRowKey } from 'rc-table/lib/interface';
-//  import React from 'react';
-//  import RequestUtil from '../utils/RequestUtil';
-//  import AbstractFilteredSelectionModal from './AbstractFilteredSelecableModal';
-//  import { IAbstractSelectableModalProps, IAbstractSelectableModalState, IResponseData } from './AbstractSelectableModal';
- 
-//  export interface IClientSelectionComponentState extends IAbstractSelectableModalState {
-//      readonly tableDataSource: IClient[];
-//  }
- 
-//  export interface IClient {
-//      readonly id: number;
-//      readonly tenantId: number;
-//      readonly name: string;
-//      readonly type: number;
-//      readonly linkman: string;
-//      readonly phone: string;
-//      readonly description: string;
-//      readonly createTime: string;
-//      readonly length: number;
-//  }
- 
-//  /**
-//   * Client Selection Component
-//   */
-//  export default class ClientSelectionComponent extends AbstractFilteredSelectionModal<IAbstractSelectableModalProps, IClientSelectionComponentState> {
- 
-//      /**
-//       * @override
-//       * @description Gets state
-//       * @returns state 
-//       */
-//      protected getState(): IClientSelectionComponentState {
-//          return {
-//              ...super.getState(),
-//              tablePagination: {
-//                  current: 1,
-//                  pageSize: 10,
-//                  total: 0,
-//                  showSizeChanger: false
-//              }
-//          };
-//      }
- 
-//      public componentDidMount(): void {
-//          this.getTable({})
-//      }
-     
-//      public async getTable(filterValues: Record<string, any>, pagination: TablePaginationConfig = {}, flag: boolean = true) {
-//          const searchValues = flag ? filterValues : {};
-//          const resData: IResponseData = await RequestUtil.get<IResponseData>('/tower-system/employee', {
-//              ...searchValues,
-//              current: pagination.current || this.state.tablePagination.current,
-//              size: pagination.pageSize || this.state.tablePagination.pageSize
-//          });
-//          this.setState({
-//              ...filterValues,
-//              tableDataSource: resData.records,
-//              tablePagination: {
-//                  ...this.state.tablePagination,
-//                  current: resData.current,
-//                  pageSize: resData.size,
-//                  total: resData.total
-//              }
-//          });
-//      }
- 
-//      public getFilterFormItemProps(): FormItemProps[] {
-//          return [{
-//              label: "用户姓名",
-//              name: 'fuzzyQuery',
-//              children: <Input placeholder="请输入用户姓名" />
-//          }];
-//      }
- 
-//      public onFilterSubmit = async (values: Record<string, any>) => {
-//          this.getTable(values, {
-//              current: 1,
-//              pageSize: 10,
-//              total: 0,
-//              showSizeChanger: false
-//          });
-//      }
- 
-//      public getTableDataSource(): object[] {
-//          return this.state.tableDataSource;
-//      }
- 
- 
-//      public getTableColumns(): ColumnType<object>[] {
-//          return [{
-//              key: 'account',
-//              title: '登录账号',
-//              dataIndex: 'account'
-//          }, {
-//              key: 'name',
-//              title: '用户姓名',
-//              dataIndex: 'name'
-//          }, {
-//              key: 'roleName',
-//              title: '所属角色',
-//              dataIndex: 'roleName'
-//          }, {
-//             key: 'deptName',
-//             title: '所属机构',
-//             dataIndex: 'deptName'
-//         }];
-//      }
- 
-//      protected getTableRowKey(): string | GetRowKey<object> {
-//          return 'id';
-//      }
-    // public render(): React.ReactNode {
-    //     return (
-    //         <>
-    //             <Button type={this.props.buttonType || 'primary'} style={this.props.buttonType==='link'?{ paddingBottom: '0', paddingTop: '0', height: 'auto', lineHeight: 1 }:{}} onClick={()=>{
-    //                 this.getTable({})
-    //                 this.setState({
-    //                     isModalVisible: true
-    //                 })
-    //             }}>{this.props.buttonTitle || '添加人员'}</Button>
-    //             <Modal
-    //                 title={this.state.confirmTitle}
-    //                 visible={this.state.isModalVisible}
-    //                 okText={this.state.okText}
-    //                 cancelText={this.state.cancelText}
-    //                 onOk={
-    //                     () => {
-    //                         this.setState({
-    //                             isModalVisible: false,
-    //                         })
-    //                         if (this.state.tableDataSource.length > 0) {
-                            
-    //                             this.getForm()?.resetFields();
-    //                             this.props.onSelect(this.state.selectedRows)
-    //                             this.setState({
-    //                                 selectedRowKeys: [],
-    //                                 selectedRows:[],
-    //                             })
-    //                         }
-    //                     }
-    //                 }
-    //                 onCancel={this.handleCancel}
-    //                 width="80%"
-    //             >
-    //                 <Space direction="vertical" className={styles.modalTable}>
-    //                     {this.renderTableContent()}
-    //                 </Space>
-    //             </Modal>
-    //         </>
-    //     );
-    // }
-//  }
 /**
  * @author lxy
  * @copyright © 2021
@@ -234,7 +75,6 @@
      public getTableProps(): TableProps<object> {
          return {
              ...super.getTableProps(),
-             pagination: false,
              size:'small',
              rowSelection: {
                  type: this.props.rowSelectionType || 'radio',
@@ -287,9 +127,9 @@
              selectedRowKeys: this.props.selectKey||[],
              tablePagination: {
                  ...this.state.tablePagination,
-                 // current: resData.current,
-                 // pageSize: resData.size,
-                 // total: resData.total
+                 current: resData.current,
+                 pageSize: resData.size,
+                 total: resData.total
              }
          });
      }
@@ -305,10 +145,10 @@
      //查询
      public onFilterSubmit = async (values: Record<string, any>) => {
          this.getTable(values, {
-             // current: 1,
-             // pageSize: 10,
-             // total: 0,
-             // showSizeChanger: false
+             current: 1,
+             pageSize: 10,
+             total: 0,
+             showSizeChanger: false
          });
      }
      //dataSource
