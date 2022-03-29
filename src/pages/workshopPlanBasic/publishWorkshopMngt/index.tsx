0160637a177253a74ecb7a2f3d1dbda8b2fc37ba
lvxianlei <lvxianlei@hotmail.com>
2022-03-29 16:57:26 +0800

    分配车间配置-联调

--- src/pages/workshopPlanBasic/publishWorkshopMngt/index.tsx
+++ src/pages/workshopPlanBasic/publishWorkshopMngt/index.tsx
@@ -12,7 +12,11 @@ export default function Index(): React.ReactElement {
     const { loading, data } = useRequest<any>(() => new Promise(async (resole, reject) => {
         try {
             const result: any = await RequestUtil.get(`/tower-aps/workshop/config/list`);
-            resole(result)
+            resole(result?.records.map((item: any) => ({
+                ...item,
+                productTypeId: item.productTypeId?.split(",") || [],
+                workshopId: item.workshopId?.split(",") || []
+            })))
         } catch (error) {
             reject(error)
         }
@@ -38,15 +42,15 @@ export default function Index(): React.ReactElement {
 
     const handleSubmit = async () => {
         const submitData = await form.validateFields()
-        const groupIds = data?.records.filter((item: any) => !(submitData.submit.map((sItem: any) => sItem.id).includes(item.id)))
+        const groupIds = data?.filter((item: any) => (submitData.submit.map((sItem: any) => sItem.id).includes(item.groupId)))
         await saveRun({
-            groupIds: groupIds.map((item: any) => item.id),
+            groupIds: groupIds.map((item: any) => item.groupId),
             workshopConfigs: submitData.submit.map((item: any) => ({
-                productTypeId: item.productTypeName,
-                weldingTypeId: item.weldingTypeName,
-                weldingWorkshopId: item.weldingWorkshopName,
-                workshopId: item.workshopName,
-                factoryId: item.factoryName
+                productTypeId: item.productTypeId.join(","),
+                weldingTypeId: item.weldingTypeId,
+                weldingWorkshopId: item.weldingWorkshopId,
+                workshopId: item.workshopId.join(","),
+                factoryId: item.factoryId
             }))
         })
         message.success("保存成功...")
@@ -60,25 +64,26 @@ export default function Index(): React.ReactElement {
                 opration={[
                     <Button type="primary" loading={saveLoading} key="edit" onClick={handleSubmit}>保存</Button>
                 ]}
+                rowKey="groupId"
                 columns={pageTableSetting.map((item: any) => {
                     switch (item.dataIndex) {
-                        case "productTypeName":
+                        case "productTypeId":
                             return ({
                                 ...item,
                                 mode: "multiple",
                                 allowClear: true,
                                 enum: productTypeOptions?.map((item: any) => ({ label: item.name, value: item.id }))
                             })
-                        case "factoryName":
+                        case "factoryId":
                             return ({ ...item, enum: factoryTypeOptions?.map((item: any) => ({ label: item.name, value: item.id })) })
                         case "weldingTypeName":
                             return ({ ...item, enum: compoundTypeOptions?.map((item: any) => ({ label: item.name, value: item.id })) })
-                        case "weldingWorkshopName":
+                        case "weldingWorkshopId":
                             return ({
                                 ...item,
                                 enum: productionUnitData?.map((item: any) => ({ label: item.name, value: item.id }))
                             })
-                        case "workshopName":
+                        case "workshopId":
                             return ({
                                 ...item,
                                 mode: "multiple",
@@ -90,13 +95,13 @@ export default function Index(): React.ReactElement {
                     }
                 })}
                 haveIndex={false}
-                dataSource={data?.records}
+                dataSource={data || []}
             />}
             {!edit && <>
                 <Space size={16} style={{ marginBottom: 16 }}>
                     <Button type="primary" onClick={handleEditClick}>编辑</Button>
                 </Space>
-                <CommonAliTable columns={pageTable} dataSource={data?.records} />
+                <CommonAliTable columns={pageTable} dataSource={data || []} />
             </>}
         </Spin>
     </DetailContent>
