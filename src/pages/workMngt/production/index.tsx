037f1fb90c4e1eea1755ef11e4b184421337a34e
齐广广 <13641315679@163.com>
2022-03-30 14:51:38 +0800

    修改生产配料的接口

--- src/pages/workMngt/production/index.tsx
+++ src/pages/workMngt/production/index.tsx
@@ -138,7 +138,7 @@ export default function Invoicing() {
                                 className="btn-operation-link" 
                                 disabled={userId !== record.batcherId || record.loftingState === 3}
                             >
-                                <Link to={`/ingredients/production/detailed/${record.id}/${record.materialTaskCode}/${record.productCategoryName}/${record.loftingState}`}>明细</Link>
+                                <Link to={`/ingredients/production/detailed/${record.id}/${record.materialTaskCode}/${record.productCategoryName}/${record.loftingState}/${record.productionBatchNo}`}>明细</Link>
                             </Button>
                             <Button type="link" className="btn-operation-link" disabled={userId !== record.batcherId || record.loftingState !== 2}
                                 onClick={() => {
