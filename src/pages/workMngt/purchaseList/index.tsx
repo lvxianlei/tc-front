9508994116f27578853e5bf7a4e127b3396f8c07
mschange <7750509+mschanges@user.noreply.gitee.com>
2022-04-13 17:32:49 +0800

    解决bug6610、5296、4744

--- src/pages/workMngt/purchaseList/index.tsx
+++ src/pages/workMngt/purchaseList/index.tsx
@@ -14,7 +14,10 @@ export default function Invoicing() {
     const [generateVisible, setGenerateVisible] = useState<boolean>(false)
     const [generateIds, setGenerateIds] = useState<string[]>([])
     const [chooseId, setChooseId] = useState<string>("")
-    const [filterValue, setFilterValue] = useState<object>(history.location.state as object);
+    const [filterValue, setFilterValue] = useState<object>({
+        ...history.location.state as object,
+        purchaserId: sessionStorage.getItem('USER_ID'),
+    });
     const onFilterSubmit = (value: any) => {
         if (value.startPurchaseStatusUpdateTime) {
             const formatDate = value.startPurchaseStatusUpdateTime.map((item: any) => item.format("YYYY-MM-DD"))
