4c54cece59ab9b66ce5401118120e407306a8bad
lixy <826446332@qq.com>
2022-04-26 18:06:21 +0800

    【开发】周期计划

--- src/pages/planBaseSetting/cyclePlan/Detail.tsx
+++ src/pages/planBaseSetting/cyclePlan/Detail.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import { Button, Spin, Space, Popconfirm, message} from 'antd';
+import React, { useState } from 'react'
+import { Button, Spin, Space, Popconfirm, message, Form, Input, Select, DatePicker, Row, Col} from 'antd';
 import { useHistory, useParams } from 'react-router-dom';
 import { DetailContent, CommonTable, DetailTitle } from '../../common';
 import useRequest from '@ahooksjs/use-request';
 import RequestUtil from '../../../utils/RequestUtil';
+import { arrayMove, SortableContainer, SortableElement, SortableHandle } from 'react-sortable-hoc';
+const SortableItem = SortableElement((props: JSX.IntrinsicAttributes & React.ClassAttributes<HTMLTableRowElement> & React.HTMLAttributes<HTMLTableRowElement>) => <tr {...props} />);
+const SortableCon = SortableContainer((props: JSX.IntrinsicAttributes & React.ClassAttributes<HTMLTableSectionElement> & React.HTMLAttributes<HTMLTableSectionElement>) => <tbody {...props} />);
 
 
 export default function CyclePlanDetail(): React.ReactNode {
     const history = useHistory()
+    const [dataSource,setDataSource] = useState<any[]>([{id:1},{id:2}])
     const params = useParams<{ id: string }>()
+    const [form] = Form.useForm();
     const { loading, data, run } = useRequest(() => new Promise(async (resole, reject) => {
         // const data: any = await RequestUtil.get(`/tower-science/drawTask/getList?drawTaskId=${params.id}`)
         resole(data)
@@ -144,37 +149,100 @@ export default function CyclePlanDetail(): React.ReactNode {
                     }}
                     okText="确认"
                     cancelText="取消"
-                    disabled={record.state === 1}
                 >
-                    <Button type="link" disabled={record.state === 1}>删除</Button>
+                    <Button type="link" >删除</Button>
                 </Popconfirm>
         }
     ]
-    
+    const onSortEnd = (props: { oldIndex: number; newIndex: number; }) => {
+        if (props.oldIndex !== props.newIndex) {
+            const newData = arrayMove(dataSource, props.oldIndex, props.newIndex).filter(el => !!el);
+            setDataSource(newData)
+        }
+    };
+
+
+    const DraggableContainer = (props: any) => (
+        <SortableCon
+            useDragHandle
+            disableAutoscroll
+            helperClass="row-dragging"
+            onSortEnd={onSortEnd}
+            {...props}
+        />
+    );
+
+    const DraggableBodyRow = ({ ...restProps }) => {
+        const index = dataSource.findIndex((x: any) => x.index === restProps['data-row-key']);
+        return <SortableItem index={index} {...restProps} />;
+    };
+    const formItemLayout = {
+        labelCol: { span: 6 },
+        wrapperCol: { span: 16 }
+    };
     return <>
         <Spin spinning={loading}>
             <DetailContent operation={[
-                // <Space>
-                //     <Button key="goback" onClick={() => history.goBack()}>返回</Button>
-                //     <Button type="primary" ghost onClick={() => history.goBack()}>保存</Button>
-                //     <Button type="primary" ghost onClick={() => history.goBack()}>备料确认</Button>
-                //     <Popconfirm
-                //         title="下发后不可取消，是否下发周期计划？"
-                //         onConfirm={async () => {
-                //             // RequestUtil.delete(`/tower-system/notice?ids=${record.id}`)
-                //             message.success("下发成功")
-                //             history.push(``)
-                //         }}
-                //         okText="确认"
-                //         cancelText="取消"
-                //     >
-                //         <Button type="primary" ghost >周期计划下发</Button>
-                //     </Popconfirm>
-                // </Space>
+                <Space>
+                    <Button key="goback" onClick={() => history.goBack()}>返回</Button>
+                    <Button type="primary" ghost onClick={() => history.goBack()}>保存</Button>
+                    <Button type="primary" ghost onClick={() => history.goBack()}>备料确认</Button>
+                    <Popconfirm
+                        title="下发后不可取消，是否下发周期计划？"
+                        onConfirm={async () => {
+                            // RequestUtil.delete(`/tower-system/notice?ids=${record.id}`)
+                            message.success("下发成功")
+                            history.push(``)
+                        }}
+                        okText="确认"
+                        cancelText="取消"
+                    >
+                        <Button type="primary" ghost >周期计划下发</Button>
+                    </Popconfirm>
+                </Space>
             ]}>
                 <DetailTitle title="基础信息"/>
-                
-                
+                <Form form={ form } { ...formItemLayout }>
+                    <Row>
+                        <Col span={12}>
+                            <Form.Item name="dept" label="周期计划号" rules={[
+                                {
+                                    required:true,
+                                    message:"请填写周期计划部门"
+                                },
+                                {
+                                    pattern: /^[^\s]*$/,
+                                    message: '禁止输入空格',
+                                }
+                            ]}>
+                                <Input maxLength={100} disabled/>
+                            </Form.Item>
+                        </Col>
+                        <Col span={12}>
+                            <Form.Item name="assignorId" label="计划起止日期" rules={[{required:true,message:"请选择计划起止日期"}]}>
+                                <DatePicker.RangePicker format="YYYY-MM-DD" style={{ width: '100%' }} />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                   
+                    <Row>
+                        <Col span={12}>
+                            <Form.Item name="dept" label="周期计划类型" rules={[
+                                {
+                                    required:true,
+                                    message:"请选择周期计划类型"
+                                }
+                            ]}>
+                                <Input maxLength={100} disabled/>
+                            </Form.Item>
+                        </Col>
+                        <Col span={12}>
+                            <Form.Item name="dept" label="备料状态" >
+                                <Input maxLength={100} disabled/>
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                </Form>
                 <DetailTitle title="周期计划下达单"/>
                 <Space>
                     <Button type="primary" ghost onClick={() => history.goBack()}>添加下达单</Button>
@@ -188,7 +256,12 @@ export default function CyclePlanDetail(): React.ReactNode {
                         <span>总重量（t）：{}</span>
                     </Space>
                 </div>
-                {/* <CommonTable columns={columns} dataSource={[{}]} pagination={false}/> */}
+                <CommonTable columns={columns} dataSource={[...dataSource]} pagination={false}  components={{
+                    body: {
+                        wrapper: DraggableContainer,
+                        row: DraggableBodyRow,
+                    },
+                }}/>
             </DetailContent>
         </Spin>
     </>
