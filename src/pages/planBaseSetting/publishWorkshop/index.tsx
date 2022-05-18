91d8f97ee071d99a86ab01a31c214fa04257207b
lixy <826446332@qq.com>
2022-05-18 14:05:39 +0800

    【修复】bug7714

--- src/pages/planBaseSetting/publishWorkshop/index.tsx
+++ src/pages/planBaseSetting/publishWorkshop/index.tsx
@@ -204,7 +204,7 @@ export default () => {
                     value={status}
                     onChange={(event) => {
                         setStatus(event.target.value)
-                        setFilterValue({ ...filterValue, status: event.target.value })
+                        setFilterValue({ ...filterValue, status: event.target.value, current: 1 })
                     }}
                 >
                     <Radio.Button value={1}>待分配下达单</Radio.Button>
