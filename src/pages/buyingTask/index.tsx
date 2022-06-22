c903811a3dfade97e33a109652d97b19d60dbd55
lvxianlei <lvxianlei@hotmail.com>
2022-06-22 16:37:43 +0800

    采购任务-原材料；

--- src/pages/buyingTask/index.tsx
+++ src/pages/buyingTask/index.tsx
@@ -150,12 +150,12 @@ export default function RawMaterial() {
                         title: "操作",
                         dataIndex: "opration",
                         fixed: "right",
-                        width: 250,
+                        width: 80,
                         render: (_: any, records: any) => <>
                             <Button type="link" className="btn-operation-link" onClick={() => { detail(records.id) }}>任务详情</Button>
-                            <Button type="link" className="btn-operation-link" disabled={records.taskStatus !== 3} onClick={() => { setDetailId(records.id); setIsModalVisible1(true) }}>指派</Button>
+                            {/* <Button type="link" className="btn-operation-link" disabled={records.taskStatus !== 3} onClick={() => { setDetailId(records.id); setIsModalVisible1(true) }}>指派</Button>
                             <Button type="link" className="btn-operation-link" disabled={records.taskStatus !== 2} onClick={() => history.push(`/buyingTask/detail/${records.planNumber}`)}>用料清单</Button>
-                            <Button type="link" className="btn-operation-link" disabled={records.taskStatus !== 2} onClick={() => setIsModalVisible2(true)}>提交任务</Button>
+                            <Button type="link" className="btn-operation-link" disabled={records.taskStatus !== 2} onClick={() => setIsModalVisible2(true)}>提交任务</Button> */}
                         </>
                     }
                 ]}
