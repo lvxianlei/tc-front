1fc66d0cafbbb2e68837c27ff48c16a110029a57
lvxianlei <lvxianlei@hotmail.com>
2022-07-01 14:26:14 +0800

    修复营销bug

--- src/pages/project/contract/ContractList.tsx
+++ src/pages/project/contract/ContractList.tsx
@@ -2,14 +2,11 @@
  * 合同列表
  */
 import React, { useState } from 'react';
-import { 
-    Button,
-    Popconfirm,
-    Space,
-    message,
-    Modal,
-    Form,
-    Select
+import {
+  Button,
+  Popconfirm,
+  Space,
+  message
 } from 'antd';
 import { useHistory, useParams, Link } from 'react-router-dom';
 import { Page } from '../../common';
@@ -19,193 +16,204 @@ import { IResponseData } from "../../common/Page";
 import MiddleModal from '../../../components/MiddleModal';
 
 export default function ContractList(): JSX.Element {
-    const history = useHistory();
-    const [ refresh, setRefresh ] = useState<boolean>(false);
-    const params = useParams<{ id: string }>();
-    const [ filterValue, setFilterValue ] = useState({projectId: params.id});
-    const onFilterSubmit = (value: any) => {
-        value["projectId"] = params.id;
-        setFilterValue({projectId: params.id})
-        return value
-    }
+  const history = useHistory();
+  const [refresh, setRefresh] = useState<boolean>(false);
+  const params = useParams<{ id: string }>();
+  const [filterValue, setFilterValue] = useState({ projectId: params.id });
+  const onFilterSubmit = (value: any) => {
+    value["projectId"] = params.id;
+    setFilterValue({ projectId: params.id })
+    return value
+  }
 
-    const uploadChange = (event: any) => {
-        if (event.file.status === "done") {
-            if (event.file.response.code === 200) {
-            message.success("杆塔信息导入成功...")
-            }
-        }
+  const uploadChange = (event: any) => {
+    if (event.file.status === "done") {
+      if (event.file.response.code === 200) {
+        message.success("杆塔信息导入成功...")
+      }
     }
-    const columns = [
-      {
-          key: "contractNumber",
-          title: "合同编号",
-          dataIndex: "contractNumber",
-          render: (_: undefined, record: object): React.ReactNode => {
-            return (
-              <Link
-                to={`/project/contract/detail/${params.id}/${(record as IContract).id
-                  }`}
-              >
-                {(record as IContract).contractNumber}
-              </Link>
-            );
-          },
-        },
-        {
-          key: "internalNumber",
-          title: "内部合同编号",
-          dataIndex: "internalNumber",
-          render: (_: undefined, record: object): React.ReactNode => {
-            return (
-              <Link to={`/project/contract/detail/${params.id}/${(record as IContract).id}`}>
-                {(record as IContract).internalNumber}
-              </Link>
-            );
-          },
-        },
-        {
-          key: "contractName",
-          title: "合同/工程名称",
-          dataIndex: "contractName",
-        },
-        {
-          key: "contractTotalWeight",
-          title: "合同总重(吨)",
-          dataIndex: "contractTotalWeight",
-        },
-        {
-          title: "合同金额(元)",
-          dataIndex: "contractAmount",
-        },
-        {
-          title: "业主单位",
-          dataIndex: "customerCompany",
-        },
-        {
-          title: "合同签订单位",
-          dataIndex: "signCustomerName",
-        },
-        {
-          title: "签订日期",
-          dataIndex: "signContractTime",
-        },
-        {
-          title: "要求发货日期",
-          dataIndex: "deliveryTime",
-        },
-        {
-          title: "有无技术协议",
-          dataIndex: "isIta",
-          render: (text: any, records: any) => {
-            let value = "无";
-            if (text === 1) {
-              value = "原件"
-            } else if (text === 2) {
-              value = "复印件"
-            }
-            return <>{value}</>
-          }
-        },
-        {
-          title: "跟单业务员",
-          dataIndex: "salesman",
-        },
-        {
-          title: "备注",
-          dataIndex: "description",
-        },
-        {
-          title: "制单人",
-          dataIndex: "createUserName",
-        },
-        {
-          title: "制单时间",
-          dataIndex: "createTime",
+  }
+  const columns = [
+    {
+      key: "contractNumber",
+      title: "合同编号",
+      width: 140,
+      dataIndex: "contractNumber",
+      render: (_: undefined, record: object): React.ReactNode => {
+        return (
+          <Link
+            to={`/project/contract/detail/${params.id}/${(record as IContract).id
+              }`}
+          >
+            {(record as IContract).contractNumber}
+          </Link>
+        );
+      },
+    },
+    {
+      key: "internalNumber",
+      title: "内部合同编号",
+      width: 140,
+      dataIndex: "internalNumber",
+      render: (_: undefined, record: object): React.ReactNode => {
+        return (
+          <Link to={`/project/contract/detail/${params.id}/${(record as IContract).id}`}>
+            {(record as IContract).internalNumber}
+          </Link>
+        );
+      },
+    },
+    {
+      key: "contractName",
+      title: "合同/工程名称",
+      width: 100,
+      dataIndex: "contractName",
+    },
+    {
+      key: "contractTotalWeight",
+      title: "合同总重(吨)",
+      width: 100,
+      dataIndex: "contractTotalWeight",
+    },
+    {
+      title: "合同金额(元)",
+      width: 100,
+      dataIndex: "contractAmount",
+    },
+    {
+      title: "业主单位",
+      width: 100,
+      dataIndex: "customerCompany",
+    },
+    {
+      title: "合同签订单位",
+      width: 140,
+      dataIndex: "signCustomerName",
+    },
+    {
+      title: "签订日期",
+      dataIndex: "signContractTime",
+      width: 100
+    },
+    {
+      title: "要求发货日期",
+      dataIndex: "deliveryTime",
+      width: 120,
+    },
+    {
+      title: "有无技术协议",
+      dataIndex: "isIta",
+      width: 130,
+      render: (text: any, records: any) => {
+        let value = "无";
+        if (text === 1) {
+          value = "原件"
+        } else if (text === 2) {
+          value = "复印件"
         }
-    ]
-    return (
-        <>
-            <Page
-                path="/tower-market/contract/getContractPackPage"
-                sourceKey="contractList.records"
-                onFilterSubmit={onFilterSubmit}
-                filterValue={filterValue}
-                extraOperation={(data: any) => {
-                    return (<>
-                        <Button type="primary" onClick={() => {
-                            history.push(`/project/contract/new/${params.id}`);
-                        }}>新增合同</Button>
-                        <span style={{marginLeft:"20px"}}>
-                            合同重量合计：{data?.contractTotalWeight || 0.00}吨&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;合同金额合计：{data?.contractTotalAmount || 0.00}元
-                        </span>
-                    </>)
-                }}
-                columns={[
-                    {
-                        key: 'index',
-                        title: '序号',
-                        dataIndex: 'index',
-                        fixed: "left",
-                        width: 50,
-                        render: (_a: any, _b: any, index: number): React.ReactNode => (<span>{index + 1}</span>)
-                    },
-                    ...columns,
-                    {
-                        key: "operation",
-                        title: "操作",
-                        fixed: "right",
-                        dataIndex: "operation",
-                        render: (_: undefined, record: any): React.ReactNode => (
-                            <Space direction="horizontal" size="small">
-                            <Button type="link">
-                                <Link
-                                to={`/project/contract/setting/${params.id}/${(record as IContract).id}`}
-                                >
-                                编辑
-                                </Link>
-                            </Button>
-                            <Popconfirm
-                                title="要删除该合同吗？"
-                                placement="topRight"
-                                okText="确认"
-                                cancelText="取消"
-                                onConfirm={async () => {
-                                let id = (record as IContract).id;
-                                const resData: IResponseData = await RequestUtil.delete(
-                                    `/tower-market/contract?id=${id}`
-                                )
-                                if (resData) {
-                                    message.success("合同已成功删除...")
-                                    setRefresh(true);
-                                }
-                                }}
-                                disabled={(record as IContract).isRelateOrder === 1}
-                            >
-                                <Button type="link" disabled={(record as IContract).isRelateOrder === 1}>
-                                删除
-                                </Button>
-                            </Popconfirm>
-                            <Button type="link">
-                                <Link
-                                to={`/project/contract/paymentRecord/${(record as IContract).id
-                                    }/${(record as any).contractName}/${(record as IContract).signCustomerId
-                                    }/${(record as IContract).signCustomerName
-                                    }/${(record as any).contractNumber}/${params.id}`}
-                                >
-                                添加回款记录
-                                </Link>
-                            </Button>
-                            <MiddleModal onSelect={async (selectedRows: any[]): Promise<void> => {
-                                console.log(selectedRows)
-                                await RequestUtil.post(`/tower-market/contract/contractBid`,{
-                                  id: record?.id,
-                                  bidStatisticsId: selectedRows[0].id
-                                })
-                                message.success('关联成功！')
-                                history.go(0)
-                            }} projectId={record?.projectId} selectKey={record?.bidStatisticsId}/>
-                            {/* <Upload
+        return <>{value}</>
+      }
+    },
+    {
+      title: "跟单业务员",
+      dataIndex: "salesman",
+      width: 100,
+    },
+    {
+      title: "备注",
+      dataIndex: "description", width: 100,
+    },
+    {
+      title: "制单人",
+      dataIndex: "createUserName", width: 100,
+    },
+    {
+      title: "制单时间",
+      dataIndex: "createTime", width: 100,
+    }
+  ]
+  return (
+    <>
+      <Page
+        path="/tower-market/contract/getContractPackPage"
+        sourceKey="contractList.records"
+        onFilterSubmit={onFilterSubmit}
+        filterValue={filterValue}
+        extraOperation={(data: any) => {
+          return (<>
+            <Button type="primary" onClick={() => {
+              history.push(`/project/contract/new/${params.id}`);
+            }}>新增合同</Button>
+            <span style={{ marginLeft: "20px" }}>
+              合同重量合计：{data?.contractTotalWeight || 0.00}吨&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;合同金额合计：{data?.contractTotalAmount || 0.00}元
+            </span>
+          </>)
+        }}
+        columns={[
+          {
+            key: 'index',
+            title: '序号',
+            dataIndex: 'index',
+            fixed: "left",
+            width: 50,
+            render: (_a: any, _b: any, index: number): React.ReactNode => (<span>{index + 1}</span>)
+          },
+          ...columns,
+          {
+            key: "operation",
+            title: "操作",
+            fixed: "right",
+            dataIndex: "operation",
+            render: (_: undefined, record: any): React.ReactNode => (
+              <Space direction="horizontal" size="small">
+                <Button type="link">
+                  <Link
+                    to={`/project/contract/setting/${params.id}/${(record as IContract).id}`}
+                  >
+                    编辑
+                  </Link>
+                </Button>
+                <Popconfirm
+                  title="要删除该合同吗？"
+                  placement="topRight"
+                  okText="确认"
+                  cancelText="取消"
+                  onConfirm={async () => {
+                    let id = (record as IContract).id;
+                    const resData: IResponseData = await RequestUtil.delete(
+                      `/tower-market/contract?id=${id}`
+                    )
+                    if (resData) {
+                      message.success("合同已成功删除...")
+                      setRefresh(true);
+                    }
+                  }}
+                  disabled={(record as IContract).isRelateOrder === 1}
+                >
+                  <Button type="link" disabled={(record as IContract).isRelateOrder === 1}>
+                    删除
+                  </Button>
+                </Popconfirm>
+                <Button type="link">
+                  <Link
+                    to={`/project/contract/paymentRecord/${(record as IContract).id
+                      }/${(record as any).contractName}/${(record as IContract).signCustomerId
+                      }/${(record as IContract).signCustomerName
+                      }/${(record as any).contractNumber}/${params.id}`}
+                  >
+                    添加回款记录
+                  </Link>
+                </Button>
+                <MiddleModal onSelect={async (selectedRows: any[]): Promise<void> => {
+                  console.log(selectedRows)
+                  await RequestUtil.post(`/tower-market/contract/contractBid`, {
+                    id: record?.id,
+                    bidStatisticsId: selectedRows[0].id
+                  })
+                  message.success('关联成功！')
+                  history.go(0)
+                }} projectId={record?.projectId} selectKey={record?.bidStatisticsId} />
+                {/* <Upload
                                 key="sub"
                                 name="file"
                                 multiple={true}
@@ -219,13 +227,13 @@ export default function ContractList(): JSX.Element {
                                 showUploadList={false}
                                 onChange={uploadChange}
                             ><Button key="enclosure" type="link">导入杆塔信息</Button></Upload> */}
-                            </Space>
-                        ),
-                      },
-                ]}
-                refresh={ refresh }
-                searchFormItems={[]}
-            />
-        </>
-    )
+              </Space>
+            ),
+          },
+        ]}
+        refresh={refresh}
+        searchFormItems={[]}
+      />
+    </>
+  )
 }
--- src/pages/project/order/Index.tsx
+++ src/pages/project/order/Index.tsx
@@ -1,142 +1,153 @@
 /***
  * 订单列表
  */
- import React, { useState } from 'react';
- import { 
-     Button,
-     Space,
- } from 'antd';
- import { useHistory, useParams, Link } from 'react-router-dom';
- import { Page } from '../../common';
- import RequestUtil from "../../../utils/RequestUtil";
- import { IResponseData } from "../../common/Page";
- import { productTypeOptions, voltageGradeOptions } from '../../../configuration/DictionaryOptions';
- import { ITableDataItem } from "../../prom/order/SaleOrder";
- import ConfirmableButton from "../../../components/ConfirmableButton";
- import AuthorityComponent from "../../../components/AuthorityComponent";
- export default function SaleOrder(): JSX.Element {
-     const history = useHistory();
-     const [ refresh, setRefresh ] = useState<boolean>(false);
-     const params = useParams<{ id: string }>();
-     const [ filterValue, setFilterValue ] = useState({projectId: params.id});
-     const onFilterSubmit = (value: any) => {
-         value["projectId"] = params.id;
-         setFilterValue({projectId: params.id})
-         return value
-     }
-    const columns = [{ 
+import React, { useState } from 'react';
+import { Button, Space } from 'antd';
+import { useHistory, useParams, Link } from 'react-router-dom';
+import { Page } from '../../common';
+import RequestUtil from "../../../utils/RequestUtil";
+import { IResponseData } from "../../common/Page";
+import { productTypeOptions, voltageGradeOptions } from '../../../configuration/DictionaryOptions';
+import { ITableDataItem } from "../../prom/order/SaleOrder";
+import ConfirmableButton from "../../../components/ConfirmableButton";
+import AuthorityComponent from "../../../components/AuthorityComponent";
+export default function SaleOrder(): JSX.Element {
+    const history = useHistory();
+    const [refresh, setRefresh] = useState<boolean>(false);
+    const params = useParams<{ id: string }>();
+    const [filterValue, setFilterValue] = useState({ projectId: params.id });
+    const onFilterSubmit = (value: any) => {
+        value["projectId"] = params.id;
+        setFilterValue({ projectId: params.id })
+        return value
+    }
+    const columns = [{
         key: "saleOrderNumber",
         title: "销售订单编号",
         dataIndex: "saleOrderNumber",
         render: (_: undefined, record: object): React.ReactNode => {
-        return (
-            <Link
-            to={`/project/order/detail/${params.id}/${(record as ITableDataItem).id
-                }`}
-            >
-            {(record as ITableDataItem).saleOrderNumber}
-            </Link>
-        );
+            return (
+                <Link
+                    to={`/project/order/detail/${params.id}/${(record as ITableDataItem).id
+                        }`}
+                >
+                    {(record as ITableDataItem).saleOrderNumber}
+                </Link>
+            );
         },
     },
     {
         key: "taskStatus",
         title: "任务下发状态",
         dataIndex: "taskStatus",
+        width: 120,
         render: (value: number, record: object): React.ReactNode => {
-        const renderEnum: any = [
-            {
-            value: 0,
-            label: "未下发"
-            },
-            {
-            value: 1,
-            label: "部分下发"
-            },
-            {
-            value: 2,
-            label: "全部下发"
-            },
-        ]
-        return <>{renderEnum.find((item: any) => item.value === value).label}</>
+            const renderEnum: any = [
+                {
+                    value: 0,
+                    label: "未下发"
+                },
+                {
+                    value: 1,
+                    label: "部分下发"
+                },
+                {
+                    value: 2,
+                    label: "全部下发"
+                },
+            ]
+            return <>{renderEnum.find((item: any) => item.value === value).label}</>
         }
     },
     {
         key: "internalNumber",
         title: "内部合同编号",
         dataIndex: "internalNumber",
+        width: 140,
         render: (_: undefined, record: object): React.ReactNode => {
-        return (
-            <Link
-            to={`/project/contract/detail/${params.id}/${(record as ITableDataItem).contractId
-                }`}
-            >
-            {(record as ITableDataItem).internalNumber}
-            </Link>
-        );
+            return (
+                <Link
+                    to={`/project/contract/detail/${params.id}/${(record as ITableDataItem).contractId
+                        }`}
+                >
+                    {(record as ITableDataItem).internalNumber}
+                </Link>
+            );
         },
     },
     {
         key: "orderProjectName",
         title: "订单工程名称",
         dataIndex: "orderProjectName",
+        width: 120,
     },
     {
         key: "customerCompany",
         title: "业主单位",
         dataIndex: "customerCompany",
+        width: 100,
     },
     {
         key: "signCustomerName",
         title: "合同签订单位",
         dataIndex: "signCustomerName",
+        width: 120,
     },
     {
         key: "productType",
         title: "产品类型",
         dataIndex: "productType",
+        width: 100,
         render: (text: any) => <>{productTypeOptions?.find((item: any) => text === item.id)?.name}</>
     },
     {
         key: "voltageGrade",
         title: "电压等级",
         dataIndex: "voltageGrade",
+        width: 100,
         render: (text: any) => <>{voltageGradeOptions?.find((item: any) => text === item.id)?.name}</>
     },
     {
         key: "taxAmount",
         title: "订单总价",
         dataIndex: "taxAmount",
+        width: 100,
     },
     {
         key: "orderWeight",
         title: "订单重量",
         dataIndex: "orderWeight",
+        width: 100,
     },
     {
         key: "taxPrice",
         title: "订单单价",
         dataIndex: "taxPrice",
+        width: 100,
     },
     {
         key: "signContractTime",
         title: "签订日期",
         dataIndex: "signContractTime",
+        width: 100,
     },
     {
         key: "deliveryTime",
         title: "合同交货日期",
         dataIndex: "deliveryTime",
+        width: 120,
     },
     {
         key: "createUserName",
         title: "制单人",
         dataIndex: "createUserName", // createUserName修改为salesman
+        width: 100,
     },
     {
         key: "createTime",
         title: "制单时间",
         dataIndex: "createTime",
+        width: 100,
     },
     ]
     return (
@@ -151,50 +162,50 @@
                     }}>新增订单</Button>
                 </>}
                 columns={[
-                {
-                    key: 'index',
-                    title: '序号',
-                    dataIndex: 'index',
-                    fixed: "left",
-                    width: 50,
-                    render: (_a: any, _b: any, index: number): React.ReactNode => (<span>{index + 1}</span>)
-                },
+                    {
+                        key: 'index',
+                        title: '序号',
+                        dataIndex: 'index',
+                        fixed: "left",
+                        width: 50,
+                        render: (_a: any, _b: any, index: number): React.ReactNode => (<span>{index + 1}</span>)
+                    },
                     ...columns,
                     {
-                    key: "operation",
-                    title: "操作",
-                    fixed: "right",
-                    dataIndex: "operation",
-                    render: (_: undefined, record: any): React.ReactNode => (
-                        <Space direction="horizontal" size="small">
-                        <Button
-                            type="link"
-                            disabled={record.isProductGroupRef !== 0}
-                            onClick={() => history.push(`/project/order/setting/${params.id}/${(record as ITableDataItem).id}`)}>编辑</Button>
-                            <AuthorityComponent permissions="sale_order_del">
-                                <ConfirmableButton
-                                    confirmTitle="要删除该订单吗？"
+                        key: "operation",
+                        title: "操作",
+                        fixed: "right",
+                        dataIndex: "operation",
+                        render: (_: undefined, record: any): React.ReactNode => (
+                            <Space direction="horizontal" size="small">
+                                <Button
                                     type="link"
-                    
-                                    placement="topRight"
-                                    onConfirm={async () => {
-                                        let id = (record as ITableDataItem).id;
-                                        const resData: IResponseData = await RequestUtil.delete(
-                                        `/tower-market/saleOrder?id=${id}`
-                                        );
-                                        setRefresh(true);
-                                    }}
-                                >
-                                    <Button type="link" disabled={record.isProductGroupRef !== 0}>删除</Button>
-                                </ConfirmableButton>
-                            </AuthorityComponent>
-                        </Space>
-                    ),
+                                    disabled={record.isProductGroupRef !== 0}
+                                    onClick={() => history.push(`/project/order/setting/${params.id}/${(record as ITableDataItem).id}`)}>编辑</Button>
+                                <AuthorityComponent permissions="sale_order_del">
+                                    <ConfirmableButton
+                                        confirmTitle="要删除该订单吗？"
+                                        type="link"
+
+                                        placement="topRight"
+                                        onConfirm={async () => {
+                                            let id = (record as ITableDataItem).id;
+                                            const resData: IResponseData = await RequestUtil.delete(
+                                                `/tower-market/saleOrder?id=${id}`
+                                            );
+                                            setRefresh(true);
+                                        }}
+                                    >
+                                        <Button type="link" disabled={record.isProductGroupRef !== 0}>删除</Button>
+                                    </ConfirmableButton>
+                                </AuthorityComponent>
+                            </Space>
+                        ),
                     },
                 ]}
-                refresh={ refresh }
+                refresh={refresh}
                 searchFormItems={[]}
             />
         </>
     )
- }
+}
