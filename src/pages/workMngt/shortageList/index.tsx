c5d35e38f4b791918c59e39f463b756da392463a
lvxianlei <lvxianlei@hotmail.com>
2022-07-08 14:58:31 +0800

    bug 8558

--- src/pages/workMngt/shortageList/index.tsx
+++ src/pages/workMngt/shortageList/index.tsx
@@ -133,7 +133,7 @@ export default function Invoicing() {
                     onChange: (selectedRowKeys: any[]) => {
                         setGenerateIds(selectedRowKeys)
                     },
-                    getCheckboxProps: (record: any) => (record.purchasePersonId !== AuthUtil.getUserId()) && record.shortageStatusName !== "待处理"
+                    getCheckboxProps: (record: any) => !((record.purchasePersonId === AuthUtil.getUserId()) && record.shortageStatus === 1)
                 }
             }}
             searchFormItems={[
