6d4e5c1711425b573aaa2060b0a9d00caf1b3b64
lvxianlei <lvxianlei@hotmail.com>
2022-07-12 15:17:28 +0800

    authButton

--- src/pages/common/AuthButton.tsx
+++ src/pages/common/AuthButton.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Button, ButtonProps } from "antd"
-
+import { hasAuthority } from "../../hooks"
 interface AuthButtonProps extends ButtonProps {
     auth?: string
 }
 
 export default function AuthButton({ auth, ...props }: AuthButtonProps) {
-
-    return <Button {...props} />
+    if (hasAuthority(auth || "")) {
+        return <Button {...props} />
+    }
+    return null
 }
