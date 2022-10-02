0dc65d41175db74f9b69f329a914b1f14fba6ac1
lvxianlei <lvxianlei@hotmail.com>
2022-10-02 14:45:36 +0800

    流程设计

---
+++ src/pages/system-mngt/workflow/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Workflow() {
+  return (
+    <div style={{ width: "100%", height: "100%" }}>
+      <iframe
+        style={{ width: "100%", height: "100%" }}
+        src=" http://workflow-dev.dhwy.cn/#/tower/flowTodo"
+      />
+    </div>
+  );
+}
