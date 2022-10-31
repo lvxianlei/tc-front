d17914fd4778ebd1fb7817661d1856a5eed59b53
lvxianlei <lvxianlei@hotmail.com>
2022-10-31 12:46:10 +0800

    提料明细

--- src/pages/workMngt/lifting/Overview.tsx
+++ src/pages/workMngt/lifting/Overview.tsx
@@ -1,17 +1,15 @@
 import React from "react"
-import { useHistory, useParams } from 'react-router-dom'
+import { useHistory, useParams, useLocation } from 'react-router-dom'
 import { DetailTitle, SearchTable as Page } from '../../common'
-import { Button, Space, Spin } from "antd"
+import { Button, Row, Space, Spin } from "antd"
 import useRequest from "@ahooksjs/use-request"
 import RequestUtil from "../../../utils/RequestUtil"
 import { liftingDetail } from "./data.json"
 export default function Invoicing() {
     const history = useHistory()
-    const { planNumber, orderProjectName } = useParams<{
-        planNumber: string
-        orderProjectName: string
-    }>()
-
+    const location = useLocation()
+    const { planNumber } = useParams<{ planNumber: string }>()
+    const orderProjectName = decodeURI(location.search).replace("?orderProjectName=", "")
     const { loading, data } = useRequest<{ [key: string]: any }>(() => new Promise(async (resolve, reject) => {
         try {
             const result: any = await RequestUtil.get(`/tower-supply/liftingSummary/summaryLiftingWeight?planNumber=${planNumber}`)
@@ -23,19 +21,23 @@ export default function Invoicing() {
 
     return <>
         <Spin spinning={loading}>
-            <DetailTitle title={<>
-                提料明细
+            <DetailTitle title="提料明细" />
+            <Row style={{
+                padding:"8px 0",
+                fontSize: 16,
+                color: "#181818",
+                fontWeight: "bold"
+            }}>
                 <span style={{
                     fontStyle: "normal",
                     fontSize: 12,
-                    marginLeft: 16
                 }}>
                     <span>计划号：<span style={{ color: "#FF8c00" }}>{planNumber}</span></span>
                     <span style={{
                         marginLeft: 16
                     }}>工程名称：<span style={{ color: "#FF8c00" }}>{orderProjectName}</span></span>
                 </span>
-            </>} />
+            </Row>
         </Spin>
         <Page
             path="/tower-supply/liftingSummary/getLiftingDetail"
