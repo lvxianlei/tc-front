faaa134fa91c995110fd3bfa80f78d505680a378
崔华 <1427703929@qq.com>
2022-12-26 16:52:15 +0800

    revise_修复

--- src/pages/project/drawing/index.tsx
+++ src/pages/project/drawing/index.tsx
@@ -199,13 +199,13 @@ export default function Drawing(): React.ReactNode {
                                 disabled={![0, 3].includes(record.auditStatus)}
                             >删除</Button>
                         </Popconfirm>
-                        {/*<Button*/}
-                        {/*    type="link"*/}
-                        {/*    size="small"*/}
-                        {/*    style={{ padding: 2 }}*/}
-                        {/*    disabled={record.auditStatus !== 1}*/}
-                        {/*    onClick={() => handleCancel(record.id)}*/}
-                        {/*>撤回</Button>*/}
+                        <Button
+                            type="link"
+                            size="small"
+                            style={{ padding: 2 }}
+                            disabled={record.auditStatus !== 1}
+                            onClick={() => handleCancel(record.id)}
+                        >撤回</Button>
                         {/*<Button*/}
                         {/*    type="link"*/}
                         {/*    size="small"*/}
