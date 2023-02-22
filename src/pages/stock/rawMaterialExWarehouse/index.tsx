d1ed51c357afe9a9ff651c718343a7aa1fb31465
lixy <826446332@qq.com>
2023-02-22 16:54:08 +0800

    【修复】12132、12136

--- src/pages/stock/rawMaterialExWarehouse/index.tsx
+++ src/pages/stock/rawMaterialExWarehouse/index.tsx
@@ -313,7 +313,7 @@ export default function RawMaterialWarehousing(): React.ReactNode {
                     {
                         name: 'fuzzyQuery',
                         label: "模糊查询",
-                        children: <Input placeholder="请输入炉批号/质保书号/下达单号/计划号/工程名称/内部合同号/塔型/备注进行查询" style={{ width: 200 }} />
+                        children: <Input placeholder="请输入收货批次/收货标识码/炉批号/质保书号/下达单号/计划号/工程名称/内部合同号/塔型/备注进行查询" style={{ width: 200 }} />
                     }
                 ]}
             />
