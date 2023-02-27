46b80adb9dd0f5f2868b0cee6cc043404f26eb14
zyc <757715809@qq.com>
2023-02-27 16:12:56 +0800

    【fix-11974】明细变更申请-增加修改项目-单重、杆塔备注

--- src/pages/businessDisposal/requestForChange/ApplyOrDetail.tsx
+++ src/pages/businessDisposal/requestForChange/ApplyOrDetail.tsx
@@ -172,6 +172,36 @@ export default forwardRef(function ApplyOrDetail({ id }: modalProps, ref) {
             render: (_: any, record: Record<string, any>): React.ReactNode => (
                 _ ? <p className={styles?.red}>{_}</p> : <p className={styles?.red}>{_}</p>
             )
+        },
+        {
+            "key": "monomerWeight",
+            "title": "单重（修改前）",
+            width: 80,
+            dataIndex: "monomerWeight"
+        },
+        {
+            "key": "changeMonomerWeight",
+            "title": "单重（修改后）",
+            width: 80,
+            dataIndex: "changeMonomerWeight",
+            render: (_: any, record: Record<string, any>): React.ReactNode => (
+                _ ? <p className={styles?.red}>{_}</p> : <p className={styles?.red}>{_}</p>
+            )
+        },
+        {
+            "key": "productDescription",
+            "title": "备注（修改前）",
+            width: 80,
+            dataIndex: "productDescription"
+        },
+        {
+            "key": "changeProductDescription",
+            "title": "备注（修改后）",
+            width: 80,
+            dataIndex: "changeProductDescription",
+            render: (_: any, record: Record<string, any>): React.ReactNode => (
+                _ ? <p className={styles?.red}>{_}</p> : <p className={styles?.red}>{_}</p>
+            )
         }
     ]
 
